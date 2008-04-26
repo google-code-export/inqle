a4823dbd989c38486f7f8e85ee6e860069aa6fdd
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2008-04-26 03:08:38 +0000

    minor

--- trunk/org.inqle.experiment.rapidminer/src/main/org/inqle/experiment/rapidminer/RapidMinerExperimentFactory.java
+++ trunk/org.inqle.experiment.rapidminer/src/main/org/inqle/experiment/rapidminer/RapidMinerExperimentFactory.java
@@ -8,7 +8,7 @@ public class RapidMinerExperimentFactory {
 		RapidMinerExperiment experiment = new RapidMinerExperiment();
 		experiment.setId(extensionSpec.getAttribute(IRapidMinerExperiment.ID_ATTRIBUTE));
 		experiment.setExperimentClassPath(extensionSpec.getAttribute(IRapidMinerExperiment.FILE_ATTRIBUTE));
-		experiment.setName(extensionSpec.getAttribute(IRapidMinerExperiment.FILE_ATTRIBUTE));
+		experiment.setName(extensionSpec.getAttribute(IRapidMinerExperiment.NAME_ATTRIBUTE));
 		experiment.setDescription(extensionSpec.getAttribute(IRapidMinerExperiment.DESCRIPTION_ATTRIBUTE));
 		experiment.setExperimentType(extensionSpec.getAttribute(IRapidMinerExperiment.TYPE_ATTRIBUTE));
 		return experiment;
