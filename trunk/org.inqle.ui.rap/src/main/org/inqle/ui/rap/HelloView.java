6886a95a3ce7b8e910664905cd8f4a0a0a8d94cc
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2008-05-02 22:22:16 +0000

    minor

--- trunk/org.inqle.ui.rap/src/main/org/inqle/ui/rap/HelloView.java
+++ trunk/org.inqle.ui.rap/src/main/org/inqle/ui/rap/HelloView.java
@@ -1,22 +1,17 @@
 package org.inqle.ui.rap;
 
 import java.util.Iterator;
-import java.util.List;
 
 import org.eclipse.core.runtime.Platform;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Link;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.ISelectionListener;
 import org.eclipse.ui.IWorkbenchPart;
