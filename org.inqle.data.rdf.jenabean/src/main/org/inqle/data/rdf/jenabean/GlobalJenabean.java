dc10cccbfacac1f66cc073743c049bf25e01db54
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2008-05-11 17:48:52 +0000

    GlobalJenabean now using hashing method to determine the ID, using SHA-256

--- org.inqle.data.rdf.jenabean/src/main/org/inqle/data/rdf/jenabean/GlobalJenabean.java
+++ org.inqle.data.rdf.jenabean/src/main/org/inqle/data/rdf/jenabean/GlobalJenabean.java
@@ -1,11 +1,19 @@
 package org.inqle.data.rdf.jenabean;
 
+import org.inqle.core.util.JavaHasher;
+
 import thewebsemantic.Id;
 
 public abstract class GlobalJenabean extends BasicJenabean implements IGlobalJenabean {
 
+//	@Id
+//	public String getId() {
+//		return super.getId();
+//	}
 	@Id
+	@Override
 	public String getId() {
-		return super.getId();
+		String hash = JavaHasher.hashSha256(toString());
+		return hash;
 	}
 }
