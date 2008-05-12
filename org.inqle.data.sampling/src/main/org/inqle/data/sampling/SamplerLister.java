0b6724b0eb98c669671e029df71f79f3f6f8c51d
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2008-05-12 21:46:00 +0000

    DataTable returns ArcSet representing learnable attribute columns fixed SamplerLister to list customizations in addition to base plugin samplers

--- org.inqle.data.sampling/src/main/org/inqle/data/sampling/SamplerLister.java
+++ org.inqle.data.sampling/src/main/org/inqle/data/sampling/SamplerLister.java
@@ -3,13 +3,49 @@ package org.inqle.data.sampling;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.inqle.core.extensions.util.ExtensionFactory;
+import org.inqle.data.rdf.AppInfo;
+import org.inqle.data.rdf.RDF;
+import org.inqle.data.rdf.jena.QueryCriteria;
+import org.inqle.data.rdf.jena.RdfTable;
+import org.inqle.data.rdf.jena.sdb.Queryer;
+import org.inqle.data.rdf.jenabean.Persister;
 import org.inqle.data.sampling.rap.ISamplerFactory;
-import org.inqle.ui.rap.IPart;
+
+import com.hp.hpl.jena.query.QuerySolution;
+import com.hp.hpl.jena.rdf.model.Literal;
 
 public class SamplerLister {
 
-	public static List<ISampler> listSamplers() {
+	private Persister persister;
+
+	static Logger log = Logger.getLogger(SamplerLister.class);
+	
+	public SamplerLister(Persister persister) {
+		this.persister = persister;
+	}
+	
+	public List<ISampler> listCustomSamplers(ISampler baseSampler) {
+		List<ISampler> customSamplers = new ArrayList<ISampler>();
+		AppInfo appInfo = persister.getAppInfo();
+		QueryCriteria queryCriteria = new QueryCriteria(persister);
+		queryCriteria.setQuery(getSparqlToFindChildren(baseSampler));
+		queryCriteria.addNamedModel(appInfo.getRepositoryNamedModel());
+		RdfTable resultTable = Queryer.selectRdfTable(queryCriteria);
+		
+		//for each item in resultTable, add a ModelPart
+		for (QuerySolution row: resultTable.getResultList()) {
+			Literal idLiteral = row.getLiteral("id");
+			log.debug("Reconstituting Sampler of class " + baseSampler.getClass() + ": " + idLiteral.getLexicalForm());
+			ISampler customSampler = (ISampler)Persister.reconstitute(baseSampler.getClass(), idLiteral.getLexicalForm(), persister.getMetarepositoryModel(), true);
+			customSamplers.add(customSampler);
+		}
+		
+		return customSamplers;
+	}
+		
+	public List<ISampler> listSamplers() {
 		List<ISampler> samplers = new ArrayList<ISampler>();
 		
 		//first add the base plugins
@@ -17,12 +53,28 @@ public class SamplerLister {
 		for (Object object: objects) {
 			if (object == null) continue;
 			ISamplerFactory samplerFactory = (ISamplerFactory)object;
-			samplers.add(samplerFactory.newSampler());
+			ISampler baseSampler = samplerFactory.newSampler();
+			samplers.add(baseSampler);
+			samplers.addAll(listCustomSamplers(baseSampler));
 		}
 		
-		//next add customized samplers
-		//TODO
-		
 		return samplers;
+	}
+	
+	
+	private static String getSparqlToFindChildren(ISampler sampler) {
+		String sparql = "PREFIX rdf: <" + RDF.RDF + ">\n" + 
+			"PREFIX ja: <" + RDF.JA + ">\n" + 
+			"PREFIX inqle: <" + RDF.INQLE + ">\n" + 
+			"SELECT ?id \n" +
+			"{\n" +
+			"GRAPH ?g {\n" +
+			"?samplerUri inqle:id ?id \n" +
+			" . ?samplerUri a ?classUri\n" +
+			" . ?classUri <" + RDF.JAVA_CLASS + "> \"" + 
+			sampler.getClass().getName()  +
+			"\" \n" +
+			"\n} }\n";
+		return sparql;
 	}
 }
