12c135543b37c180fc7926c5be70927d1c0931e1
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2008-05-19 22:37:33 +0000

    added shell script to launch

---
+++ trunk/InqleFeature/dist_root/run-inqle-console.sh
@@ -0,0 +1,5 @@
+echo starting INQLE...
+echo ----------------------------
+java -jar plugins/org.eclipse.osgi_3.3.2.R33x_v20080105.jar -configuration configuration -consoleLog -debug -clean -dev -console
+echo -----------------------------
+echo INQLE stopped
