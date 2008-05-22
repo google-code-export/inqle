645a50a135b71bba947adcd1497bf0213f2704b4
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2008-05-22 21:47:59 +0000

    runs on cygwin (and I assume Mac & Linux)

--- InqleFeature/dist_root/run-inqle-console.sh
+++ InqleFeature/dist_root/run-inqle-console.sh
@@ -1,5 +1,7 @@
 echo starting INQLE...
 echo ----------------------------
-java -jar plugins/org.eclipse.osgi_3.3.2.R33x_v20080105.jar -configuration configuration -consoleLog -debug -clean -dev -console
+java -Dorg.osgi.framework.bootdelegation=* -jar org.eclipse.osgi_3.3.2.R33x_v20080105.jar -configuration config-linux -consoleLog -debug -clean -dev -console
 echo -----------------------------
 echo INQLE stopped
+#org.eclipse.equinox.launcher_1.0.1.R33x_v20070828.jar
+#org.eclipse.osgi_3.3.2.R33x_v20080105.jar
