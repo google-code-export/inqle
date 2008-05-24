32dbae45e98ef13ee3e99ec514dcdf8c624bb922
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2008-05-24 18:07:52 +0000

    moving secure folder to _private, set Subversion to ignore _private/*

--- trunk/InqleFeature/dist_root/run-inqle-console.sh
+++ trunk/InqleFeature/dist_root/run-inqle-console.sh
@@ -1,6 +1,6 @@
 echo starting INQLE...
 echo ----------------------------
-java -Dorg.osgi.framework.bootdelegation=* -jar org.eclipse.osgi_3.3.2.R33x_v20080105.jar -configuration config-linux -consoleLog -debug -clean -dev -console
+java -Dorg.osgi.framework.bootdelegation=* -jar org.eclipse.osgi_3.3.2.R33x_v20080105.jar -configuration config-linux -consoleLog -debug -clean -console -dev
 echo -----------------------------
 echo INQLE stopped
 #org.eclipse.equinox.launcher_1.0.1.R33x_v20070828.jar
