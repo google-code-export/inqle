7f8e41bc34fc98e926a7833aacd551e908447f9a
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2008-06-03 03:12:39 +0000

    added classes for handling URI prefix mapping

---
+++ trunk/org.inqle.data.rdf.jenabean/src/main/org/inqle/data/rdf/jena/uri/UriMapping.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package org.inqle.data.rdf.jena.uri;
+
+import thewebsemantic.Namespace;
+import org.inqle.data.rdf.RDF;
+
+/**
+ * This class maps a string (e.g. a column header)
+ * to a RDF123 expression.
+ * @author David Donohue
+ * May 30, 2008
+ */
+@Namespace(RDF.INQLE)
+public class UriMapping {
+
+	private String string;
+	private String uri;
+	
+	public String getString() {
+		return string;
+	}
+	public void setString(String string) {
+		this.string = string;
+	}
+	public String getUri() {
+		return uri;
+	}
+	public void setUri(String uri) {
+		this.uri = uri;
+	}
+}
