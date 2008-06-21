f4895c009002e16c53107492ce92855e6c75a090
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2008-06-21 11:07:52 +0000

    setup wizard supports forms for embedded database

---
+++ trunk/org.inqle.ui.rap/src/main/org/inqle/ui/rap/pages/RadiosPage.java
@@ -0,0 +1,66 @@
+package org.inqle.ui.rap.pages;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.RowLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+
+public class RadiosPage extends WizardPage {
+
+//	private Composite composite;
+
+	private List<Button> buttons = new ArrayList<Button>();
+	private List<String> radioOptionTexts = new ArrayList<String>();
+	
+	public RadiosPage(String pageName) {
+		this(pageName, pageName, null);
+	}
+	
+	public RadiosPage(String pageName, String title, ImageDescriptor titleImage) {
+		super(pageName, title, titleImage);
+	}
+
+	public void createControl(Composite parent) {
+		Composite composite = new Composite(parent, SWT.NONE);
+		composite.setLayout (new RowLayout (SWT.VERTICAL));
+		
+		int index = 0;
+		for (String radioOptionText: getRadioOptionTexts()) {
+			Button button = new Button (composite, SWT.RADIO);
+			button.setText (radioOptionText);
+			if (index == 0) {
+				button.setSelection (true);
+			}
+			buttons.add(button);
+			index++;
+		}
+		setControl(composite);
+	}
+
+	private List<String> getRadioOptionTexts() {
+		return radioOptionTexts;
+	}
+
+	public void setRadioOptionTexts(List<String> radioOptionTexts) {
+		this.radioOptionTexts = radioOptionTexts;
+	}
+
+	public int getSelectedIndex() {
+		int selectedIndex = -1;
+		int index = 0;
+		for (Button button: buttons) {
+			if (button.getSelection()) {
+				selectedIndex = index;
+			}
+			index++;
+		}
+		return selectedIndex;
+	}
+
+
+}
