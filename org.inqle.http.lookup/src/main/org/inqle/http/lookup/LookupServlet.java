7ac130776b96a8cc8060eb53cff04d38b73f3807
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2008-07-10 02:33:10 +0000

    minor if anything

--- org.inqle.http.lookup/src/main/org/inqle/http/lookup/LookupServlet.java
+++ org.inqle.http.lookup/src/main/org/inqle/http/lookup/LookupServlet.java
@@ -18,10 +18,17 @@ public class LookupServlet extends HttpServlet {
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) {
 		log.info("RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRReceived Get request: " + request);
+		doWork(request, response);
 	}
 	
 	@Override
 	public void doPost(HttpServletRequest request, HttpServletResponse response) {
 		log.info("RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRReceived Post request: " + request);
+		doWork(request, response);
+	}
+
+	private void doWork(HttpServletRequest request, HttpServletResponse response) {
+		
+		
 	}
 }
