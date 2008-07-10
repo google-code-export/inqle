a2d9bc6b756955c8cfe61f8ce545b34e62fb0af4
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2008-07-10 02:34:58 +0000

    added DataSubject added method to JenabeanWriter

---
+++ org.inqle.data.rdf.jenabean/src/main/org/inqle/data/rdf/DataSubject.java
@@ -0,0 +1,10 @@
+package org.inqle.data.rdf;
+
+import thewebsemantic.Namespace;
+
+@Namespace(RDF.INQLE)
+public class DataSubject {
+
+	
+
+}
--- org.inqle.data.rdf.jenabean/src/test/org/inqle/test/data/TestJenabean.java
+++ org.inqle.data.rdf.jenabean/src/test/org/inqle/test/data/TestJenabean.java
@@ -1,7 +1,5 @@
 package org.inqle.test.data;
 
-import static org.junit.Assert.*;
-
 import java.util.Arrays;
 import java.util.List;
 
