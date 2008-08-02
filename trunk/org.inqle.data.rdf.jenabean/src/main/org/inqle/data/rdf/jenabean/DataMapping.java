f48333a84c7416b7b1911e919f6419278965888b
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2008-08-02 05:42:33 +0000

    renamed properties; planning to use this class for lookups of properties for a given owl class

--- trunk/org.inqle.data.rdf.jenabean/src/main/org/inqle/data/rdf/jenabean/DataMapping.java
+++ trunk/org.inqle.data.rdf.jenabean/src/main/org/inqle/data/rdf/jenabean/DataMapping.java
@@ -5,6 +5,8 @@ import java.net.URI;
 import org.inqle.data.rdf.RDF;
 import org.inqle.data.rdf.jena.TargetDataset;
 
+import com.hp.hpl.jena.rdf.model.Resource;
+
 import thewebsemantic.Namespace;
 
 /**
@@ -15,20 +17,20 @@ import thewebsemantic.Namespace;
  * A DataMapping object can specify that a value should populate an RDF attribute, which
  * is in any of 4 types of locations
  * (1) an anonymous object of a native class, like foaf:Person
- * In this case, the attribute uriOfMappedNativeClass is populated with the URI of the class
+ * In this case, the attribute mapsOwlClass is populated with the URI of the class
  * (2) an specific object of a native class, like foaf:Person#David_Donohue
- * In this case, the attribute uriOfMappedNativeClass is populated with the URI of the class,
- * The attribute uriOfMappedNativeObject is populated with the URI of the object
+ * In this case, the attribute mapsOwlClass is populated with the URI of the class,
+ * The attribute mapsOwlObject is populated with the URI of the object
  * (3) An anonymous object of a subclass of inqle:Data, which has inqle:subject = an anonymous
  * object of a native class.
- * In this case, the attribute uriOfWrappedClass is populated with the URI of the native class, 
+ * In this case, the attribute mapsDataAboutOwlClass is populated with the URI of the native class, 
  * which the inqle:Data object is about.
  * (4) An anonymous object of a subclass of inqle:Data, which has inqle:subject a specific object
  * of a native class.
- * In this case, the attribute uriOfWrappedClass is populated with the URI of the native class, 
+ * In this case, the attribute mapsDataAboutOwlClass is populated with the URI of the native class, 
  * which the inqle:Data object is about.
- * The attribute uriOfWrappedObject is populated with the URI of the object of class specified by 
- * uriOfWrappedClass.
+ * The attribute mapsDataAboutOwlObject is populated with the URI of the object of class specified by 
+ * mapsDataAboutOwlClass.
  * 
  * @author David Donohue
  * Jul 12, 2008
@@ -40,32 +42,32 @@ import thewebsemantic.Namespace;
 public class DataMapping extends GlobalJenabean {
 
 	public static final String MAPPING_DATASET_ROLE_ID = "org.inqle.datasets.mapping";
-	private String mappedText;
-	private URI mappedPredicate;
-	private URI uriOfMappedNativeClass;
-	private URI uriOfMappedNativeObject;
-	private URI uriOfWrappedClass;
-	private URI uriOfWrappedObject;
+	private String mapsText;
+	private Resource mapsPredicate;
+	private Resource mapsOwlClass;
+	private Resource mapsOwlObject;
+	private Resource mapsDataAboutOwlClass;
+	private Resource mapsDataAboutOwlObject;
 	
 	public String getStringRepresentation() {
 		String s = getClass().toString() + " {\n";
-		s += "[mappedText=" + mappedText + "]\n";
-		s += "[mappedPredicate=" + mappedPredicate + "]\n";
-		s += "[uriOfMappedNativeClass=" + uriOfMappedNativeClass + "]\n";
-		s += "[uriOfMappedNativeObject=" + uriOfMappedNativeObject + "]\n";
-		s += "[uriOfWrappedClass=" + uriOfWrappedClass + "]\n";
-		s += "[uriOfWrappedObject=" + uriOfWrappedObject + "]\n";
+		s += "[mapsText=" + mapsText + "]\n";
+		s += "[mapsPredicate=" + mapsPredicate + "]\n";
+		s += "[mapsOwlClass=" + mapsOwlClass + "]\n";
+		s += "[mapsOwlObject=" + mapsOwlObject + "]\n";
+		s += "[mapsDataAboutOwlClass=" + mapsDataAboutOwlClass + "]\n";
+		s += "[mapsDataAboutOwlObject=" + mapsDataAboutOwlObject + "]\n";
 		s += "}";
 		return s;
 	}
 
 	public void clone(DataMapping objectToBeCloned) {
-		setMappedText(objectToBeCloned.getMappedText());
-		setMappedPredicate(objectToBeCloned.getMappedPredicate());
-		setUriOfMappedNativeClass(objectToBeCloned.getUriOfMappedNativeClass());
-		setUriOfMappedNativeObject(objectToBeCloned.getUriOfMappedNativeObject());
-		setUriOfWrappedClass(objectToBeCloned.getUriOfWrappedClass());
-		setUriOfWrappedObject(objectToBeCloned.getUriOfWrappedObject());
+		setMapsText(objectToBeCloned.getMapsText());
+		setMapsPredicate(objectToBeCloned.getMapsPredicate());
+		setMapsOwlClass(objectToBeCloned.getMapsOwlClass());
+		setMapsOwlObject(objectToBeCloned.getMapsOwlObject());
+		setMapsDataAboutOwlClass(objectToBeCloned.getMapsDataAboutOwlClass());
+		setMapsDataAboutOwlObject(objectToBeCloned.getMapsDataAboutOwlObject());
 		super.clone(objectToBeCloned);
 	}
 	
@@ -75,52 +77,52 @@ public class DataMapping extends GlobalJenabean {
 		return dataMapping;
 	}
 
-	public String getMappedText() {
-		return mappedText;
+	public String getMapsText() {
+		return mapsText;
 	}
 
-	public void setMappedText(String mappedText) {
-		this.mappedText = mappedText;
+	public void setMapsText(String mapsText) {
+		this.mapsText = mapsText;
 	}
 
-	public URI getMappedPredicate() {
-		return mappedPredicate;
+	public Resource getMapsPredicate() {
+		return mapsPredicate;
 	}
 
-	public void setMappedPredicate(URI mappedPredicate) {
-		this.mappedPredicate = mappedPredicate;
+	public void setMapsPredicate(Resource mapsPredicate) {
+		this.mapsPredicate = mapsPredicate;
 	}
 
-	public URI getUriOfMappedNativeClass() {
-		return uriOfMappedNativeClass;
+	public Resource getMapsOwlClass() {
+		return mapsOwlClass;
 	}
 
-	public void setUriOfMappedNativeClass(URI uriOfMappedNativeClass) {
-		this.uriOfMappedNativeClass = uriOfMappedNativeClass;
+	public void setMapsOwlClass(Resource mapsOwlClass) {
+		this.mapsOwlClass = mapsOwlClass;
 	}
 
-	public URI getUriOfMappedNativeObject() {
-		return uriOfMappedNativeObject;
+	public Resource getMapsOwlObject() {
+		return mapsOwlObject;
 	}
 
-	public void setUriOfMappedNativeObject(URI uriOfMappedNativeObject) {
-		this.uriOfMappedNativeObject = uriOfMappedNativeObject;
+	public void setMapsOwlObject(Resource mapsOwlObject) {
+		this.mapsOwlObject = mapsOwlObject;
 	}
 
-	public URI getUriOfWrappedClass() {
-		return uriOfWrappedClass;
+	public Resource getMapsDataAboutOwlClass() {
+		return mapsDataAboutOwlClass;
 	}
 
-	public void setUriOfWrappedClass(URI uriOfWrappedClass) {
-		this.uriOfWrappedClass = uriOfWrappedClass;
+	public void setMapsDataAboutOwlClass(Resource mapsDataAboutOwlClass) {
+		this.mapsDataAboutOwlClass = mapsDataAboutOwlClass;
 	}
 
-	public URI getUriOfWrappedObject() {
-		return uriOfWrappedObject;
+	public Resource getMapsDataAboutOwlObject() {
+		return mapsDataAboutOwlObject;
 	}
 
-	public void setUriOfWrappedObject(URI uriOfWrappedObject) {
-		this.uriOfWrappedObject = uriOfWrappedObject;
+	public void setMapsDataAboutOwlObject(Resource mapsDataAboutOwlObject) {
+		this.mapsDataAboutOwlObject = mapsDataAboutOwlObject;
 	}
 
 	
