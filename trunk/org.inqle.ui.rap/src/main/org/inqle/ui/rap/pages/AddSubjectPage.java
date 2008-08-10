910e8220cd236b579ecfca62c714e16d830c18de
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2008-08-10 04:29:15 +0000

    setting up FileDataImporterWizard to add 5 pages upon selecting 1 of 2 radio buttons for 2 types of subjects
    need to add page for doing the import
    need to delete this page before adding new subject pages, and add it to the end of the new subject pages

--- trunk/org.inqle.ui.rap/src/main/org/inqle/ui/rap/pages/AddSubjectPage.java
+++ trunk/org.inqle.ui.rap/src/main/org/inqle/ui/rap/pages/AddSubjectPage.java
@@ -1,29 +1,31 @@
 package org.inqle.ui.rap.pages;
 
+import org.eclipse.jface.wizard.IWizard;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
+import org.inqle.ui.rap.actions.FileDataImporterWizard;
 
-public abstract class AddSubjectPage extends DynaWizardPage {
+public class AddSubjectPage extends DynaWizardPage implements SelectionListener {
 
 	private static final String PAGE_TITLE = "Add a Subject";
 	private static final String PAGE_DESCRIPTION = null;
-	private static final String FINISH_WIZARD_TEXT = "Finished adding subjects.  Ready to finish wizard and import the data.";
 	private static final String ROW_SUBJECT_BUTTON_TEXT = "Add multiple subjects, each pertaining to different rows.";
 	private static final String TABLE_SUBJECT_BUTTON_TEXT = "Add a single subject, which pertains to all rows.";
 
 	public AddSubjectPage() {
-		super(PAGE_TITLE, null);
-		setDescription(PAGE_DESCRIPTION);
+		this(PAGE_TITLE, PAGE_DESCRIPTION);
 	}
 
-	public abstract String getPageDescription();
+	public AddSubjectPage(String pageTitle, String pageDescription) {
+		super(pageTitle, null);
+		setDescription(pageDescription);
+	}
 
-	public abstract String getPageTitle();
-
-	private Button newTableSubjectButton;
-	private Button newRowSubjectButton;
-	private Button finishWizardButton;
+	protected Button newTableSubjectButton;
+	protected Button newRowSubjectButton;
 
 	@Override
 	public void addElements() {
@@ -33,12 +35,51 @@ public abstract class AddSubjectPage extends DynaWizardPage {
 		newTableSubjectButton = new Button (selfComposite, SWT.RADIO);
 		newTableSubjectButton.setText(TABLE_SUBJECT_BUTTON_TEXT);
 		newTableSubjectButton.setSelection(true);
+		newTableSubjectButton.addSelectionListener(this);
 		
 		newRowSubjectButton = new Button (selfComposite, SWT.RADIO);
 		newRowSubjectButton.setText(ROW_SUBJECT_BUTTON_TEXT);
+		newRowSubjectButton.addSelectionListener(this);
 		
-		finishWizardButton = new Button (selfComposite, SWT.RADIO);
-		finishWizardButton.setText(FINISH_WIZARD_TEXT);
+	}
+
+	public void widgetDefaultSelected(SelectionEvent selectionEvent) {
+	}
+
+	public void widgetSelected(SelectionEvent selectionEvent) {
+		Object clickedObject = selectionEvent.getSource();
+		if (clickedObject.equals(newTableSubjectButton)) {
+			newRowSubjectButton.setSelection(false);
+		}
+		if (clickedObject.equals(newRowSubjectButton)) {
+			newTableSubjectButton.setSelection(false);
+		}
+	}
+
+	@Override
+	public boolean onNextPage() {
+		if (newTableSubjectButton.getSelection()) {
+			disableForm();
+			getFileDataImporterWizard().addTableSubjectPages();
+		}
+		if (newRowSubjectButton.getSelection()) {
+			disableForm();
+			getFileDataImporterWizard().addRowSubjectPages();
+		}
+		return super.onNextPage();
+	}
+	
+	protected FileDataImporterWizard getFileDataImporterWizard() {
+		IWizard wizard = getWizard();
+		if (wizard != null && wizard instanceof FileDataImporterWizard) {
+			return (FileDataImporterWizard)wizard;
+		}
+		return null;
+	}
+
+	protected void disableForm() {
+		newTableSubjectButton.setEnabled(false);
+		newRowSubjectButton.setEnabled(false);
 	}
 
 }
