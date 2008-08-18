caf1fb2bd05eaa1c2043731823c2b160133d4d0e
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2008-08-18 03:59:17 +0000

    moving XML methods to XmlDocumentUtil

---
+++ trunk/org.inqle.core/src/org/inqle/core/util/XmlDocumentUtil.java
@@ -0,0 +1,56 @@
+package org.inqle.core.util;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.apache.log4j.Logger;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import com.sun.org.apache.xml.internal.serialize.OutputFormat;
+import com.sun.org.apache.xml.internal.serialize.XMLSerializer;
+
+public class XmlDocumentUtil {
+
+	public static Logger log = Logger.getLogger(XmlDocumentUtil.class);
+	
+	public static Document getDocument(String xmlString) {
+		ByteArrayInputStream in = new ByteArrayInputStream(xmlString.getBytes());
+		return getDocument(in);
+	}
+	
+	public static Document getDocument(InputStream in) {
+		Document document = null;
+		try {
+			DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+			document = builder.parse(in);
+		} catch (Exception e) {
+			log.error("Unable to build/parse XML from local SPARQL query", e);
+		}
+		return document;
+	}
+	
+	public static String xmlToString(Element element) {
+		String xmlString = null;
+		OutputFormat outputFormat = new OutputFormat("XML","ISO-8859-1",true);
+		outputFormat.setIndent(1);
+		outputFormat.setIndenting(true);
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		XMLSerializer serializer = new XMLSerializer(outputStream, outputFormat);
+		// As a DOM Serializer
+		try {
+			serializer.asDOMSerializer();
+			serializer.serialize( element );
+			xmlString = new String(outputStream.toByteArray());
+		} catch (IOException e) {
+			log.error("Unable to serialize received XML Document", e);
+			return null;
+		}
+		return xmlString;
+	}
+}
