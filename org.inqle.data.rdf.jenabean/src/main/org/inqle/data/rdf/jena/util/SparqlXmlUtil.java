f0e73288e574cf8e4ff5f7b77fbe5a4efdd3af23
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2008-08-19 03:26:35 +0000

    added 4 types of property pages, but not yet tested/working

--- org.inqle.data.rdf.jenabean/src/main/org/inqle/data/rdf/jena/util/SparqlXmlUtil.java
+++ org.inqle.data.rdf.jenabean/src/main/org/inqle/data/rdf/jena/util/SparqlXmlUtil.java
@@ -1,5 +1,10 @@
 package org.inqle.data.rdf.jena.util;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.log4j.Logger;
 import org.inqle.core.util.XmlDocumentSerializer;
 import org.w3c.dom.Document;
@@ -7,9 +12,9 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-public class SparqlXmlMerger {
+public class SparqlXmlUtil {
 
-	private static Logger log = Logger.getLogger(SparqlXmlMerger.class);
+	private static Logger log = Logger.getLogger(SparqlXmlUtil.class);
 	/**
 	 * This method merges the results of 2 SPARQL
 	 * queries, each having been converted to XML format
@@ -25,27 +30,35 @@ public class SparqlXmlMerger {
 	public static Document merge(Document originalDocument, Document addDocument) {		
 		if (addDocument == null) return originalDocument;
 		if (originalDocument == null) return addDocument;
+		NodeList originalDocRows = addDocument.getElementsByTagName("result");
+		if (originalDocRows == null || originalDocRows.getLength()==0) {
+			return addDocument;
+		}
 		
 		log.info("Merging XML Documents: FIRST\n" + XmlDocumentSerializer.xmlToString(originalDocument));
 		log.info("Merging XML Documents: SECOND\n" + XmlDocumentSerializer.xmlToString(addDocument));
 		
 		Document doc = originalDocument;
 		try {
-			NodeList resultsNL = originalDocument.getElementsByTagName("results");
+			log.info("1: originalDocument==null?" + (originalDocument==null));
+			Object resultsObj = originalDocument.getElementsByTagName("results");
+			log.info("2...");
+			NodeList resultsNL = (NodeList)resultsObj;
+			log.info("3...");
 			Element resultsElement = (Element)resultsNL.item(0);
-			
+			log.info("4...");
 			NodeList addRows = addDocument.getElementsByTagName("result");
-			
+			log.info("5...");
 			//get list of <result> tags from the 2nd set of results, to be added
 //			NodeList addResults = originalDocument.getElementsByTagName("results");
 //			Element addResultsElement = (Element)originalResults.item(0);
 //			NodeList addRows = addResultsElement.getElementsByTagName("result");
 			for (int i=0; i<addRows.getLength(); i++) {
-				
+				log.info("i=" + i);
 				Object addRowObject = addRows.item(i);
 				Node addRowNode = (Node)addRowObject;
 //				Node newAddRowNode = addRowNode.cloneNode(true);
-//				log.info("Adding this node:" + addRowNode.getNodeName() + "=" + addRowNode.getTextContent());
+				log.info("Adding this node:" + addRowNode.getNodeName() + "=" + addRowNode.getTextContent());
 				//originalResultsElement.insertBefore(newAddRowElement, null);
 				//originalResultsElement.appendChild(newAddRowElement);				
 //				doc.importNode(addRowNode, true);
@@ -87,4 +100,39 @@ public class SparqlXmlMerger {
 		log.info("Counting document results.  Found " + results.getLength());
 		return results.getLength();
 	}
+	
+	@Deprecated
+	public NodeList getRows(Document sparqlDocument) {		
+		NodeList rowNodes = sparqlDocument.getElementsByTagName("result");
+		return rowNodes;
+	}
+	
+	/**
+	 * Given a XML Document containing a SPARQL result set,
+	 * retrieves a List (one entry per row) of Maps (one entry per column)
+	 * @param sparqlDocument
+	 * @return
+	 */
+	public static List<Map<String, String>> getRowValues(Document sparqlDocument) {
+		ArrayList<Map<String, String>> rowElements = new ArrayList<Map<String, String>>();
+		NodeList results = sparqlDocument.getElementsByTagName("results");
+		Element resultsElement = (Element)results.item(0);
+		NodeList rows = resultsElement.getElementsByTagName("result");
+		
+		for (int j=0; j<rows.getLength(); j++) {
+			HashMap<String, String> valueMap = new HashMap<String, String>();
+			Element rowElement = (Element)rows.item(j);
+			NodeList bindingNodes = rowElement.getElementsByTagName("binding");
+			for (int k=0; k<bindingNodes.getLength(); k++) {
+				Element cellElement = (Element)bindingNodes.item(k);
+				String varName = cellElement.getAttribute("name");
+				String elementValue = cellElement.getTextContent();
+//				log.info("Row=" + j + ", Col=" + k + " Adding:" + varName + "=" + elementValue);
+				valueMap.put(varName, elementValue);
+			}
+			rowElements.add(valueMap);
+		}
+		return rowElements;
+	}
+	
 }
