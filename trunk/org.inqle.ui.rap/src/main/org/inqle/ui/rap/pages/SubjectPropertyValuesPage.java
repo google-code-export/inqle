76e481bbce0c7c8e7445535b2288c04570d18f95
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2008-08-26 10:48:39 +0000

    organized properties pages into hierarchy of grandparent (SubjectPropertiesPage), parent (~MappingsPage and ~ValuesPage) these have single button to create property; need to add a second one for capturing properties of inqle:Data

--- trunk/org.inqle.ui.rap/src/main/org/inqle/ui/rap/pages/SubjectPropertyValuesPage.java
+++ trunk/org.inqle.ui.rap/src/main/org/inqle/ui/rap/pages/SubjectPropertyValuesPage.java
@@ -1,92 +1,27 @@
 package org.inqle.ui.rap.pages;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
 import org.apache.log4j.Logger;
-import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.GridLayout;
-import org.inqle.core.util.SparqlXmlUtil;
-import org.inqle.core.util.XmlDocumentUtil;
-import org.inqle.http.lookup.PropertyLookup;
-import org.inqle.ui.rap.actions.FileDataImporterWizard;
-import org.inqle.ui.rap.widgets.IDataFieldShower;
 import org.inqle.ui.rap.widgets.TextFieldShower;
-import org.w3c.dom.Document;
 
-public abstract class SubjectPropertyValuesPage extends DynaWizardPage {
+public abstract class SubjectPropertyValuesPage extends SubjectPropertiesPage {
+
+	public SubjectPropertyValuesPage(String title, String description) {
+		super(title, description);
+	}
 
 	private static Logger log = Logger.getLogger(SubjectPropertyValuesPage.class);
-	
-	private String subjectClassUri;
 
-	private List<TextFieldShower> textFields;
-	
-	public SubjectPropertyValuesPage(String title, String description) {
-		super(title, null);
-		setDescription(description);
-	}
-	
-	public SubjectPropertyValuesPage(String title, ImageDescriptor titleImage) {
-		super(title, titleImage);
-	}
-
-	@Override
-	public void addElements() {
-		GridLayout gl = new GridLayout(2, false);
-		selfComposite.setLayout(gl);
-	}
-	
-	@Override
-	public void onEnterPageFromPrevious() {
-		log.info("Entering SubjectPropertyValuesPage...");
-		String currentSubjectClassUri = getSubjectUri();
-		if (currentSubjectClassUri.equals(subjectClassUri)) {
-			return;
-		}
-		subjectClassUri = currentSubjectClassUri;
-		String propertiesXml = PropertyLookup.lookupAllDataProperties(
-				subjectClassUri, 
-				10, 
-				0);
-		Document propertiesDocument = XmlDocumentUtil.getDocument(propertiesXml);
-		List<Map<String, String>> rowValues = SparqlXmlUtil.getRowValues(propertiesDocument);
-		
-		makePropertyFormElements(rowValues);
-	}
-
-	/**
-	 * Get the URI of the subject
-	 * @return
-	 */
-	private String getSubjectUri() {
-		FileDataImporterWizard wizard = (FileDataImporterWizard)getWizard();
-		return wizard.getSubjectClassUri(this);
-	}
-
-	protected void makePropertyFormElements(List<Map<String, String>> rowValues) {
-		textFields = new ArrayList<TextFieldShower>();
-		for (Map<String, String> row: rowValues) {
-			String uri = row.get(PropertyLookup.QUERY_HEADER_URI);
-			String label = row.get(PropertyLookup.QUERY_HEADER_LABEL);
-			String comment = row.get(PropertyLookup.QUERY_HEADER_COMMENT);
-			TextFieldShower textFieldShower = new TextFieldShower(
-					selfComposite,
-					label,
-					comment,
-					null,
-					SWT.BORDER
-			);
-			textFieldShower.setFieldUri(uri);
-			textFields.add(textFieldShower);
-		}
-	}
-
-	public IDataFieldShower[] getDataFields() {
-		IDataFieldShower[] dataFieldShowerArray = {};
-		return textFields.toArray(dataFieldShowerArray);
+	protected void addPropertyFormItem(String uri, String label, String comment) {
+		TextFieldShower textFieldShower = new TextFieldShower(
+			formComposite,
+			label,
+			comment,
+			null,
+			SWT.BORDER
+		);
+		textFieldShower.setFieldUri(uri);
+		dataFieldShowers.add(textFieldShower);
 	}
 
 }
