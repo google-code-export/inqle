d0ddf3c964f3f986a345cc9c1571dff2f9918899
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2008-10-07 17:39:43 +0000

    indexing of external datasets appears to be working, as does dataset functions

--- org.inqle.data.rdf.jenabean/src/main/org/inqle/data/rdf/jena/QueryCriteriaFactory.java
+++ org.inqle.data.rdf.jenabean/src/main/org/inqle/data/rdf/jena/QueryCriteriaFactory.java
@@ -1,5 +1,7 @@
 package org.inqle.data.rdf.jena;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import org.apache.log4j.Logger;
@@ -14,34 +16,48 @@ public class QueryCriteriaFactory {
 	
 	public static QueryCriteria createQueryCriteriaForDatasetFunction(String datasetFunctionId) {
 		QueryCriteria returnQueryCriteria = new QueryCriteria();
-		
+		Persister persister = Persister.getInstance();
+		returnQueryCriteria.setTextIndex(persister.getIndex(datasetFunctionId));
 		//query out the IDs of the ExternalDatasets of this function
 		
-		Persister persister = Persister.getInstance();
-		AppInfo appInfo = persister.getAppInfo();
-		QueryCriteria queryCriteria = new QueryCriteria();
-		String sparql = getSparqlToFindDatasetsOfFunction(datasetFunctionId);
-		queryCriteria.setQuery(sparql);
-		log.info("SPARQL=" + sparql);
-		queryCriteria.addNamedModel(appInfo.getMetarepositoryDataset());
-		//RdfTable resultTable = Queryer.selectRdfTable(queryCriteria);
-		List<String> datasetIds = Queryer.selectSimpleList(queryCriteria, "datasetId");
+		
+//		AppInfo appInfo = persister.getAppInfo();
+//		QueryCriteria queryCriteria = new QueryCriteria();
+//		String sparql = getSparqlToFindDatasetsOfFunction(datasetFunctionId);
+//		queryCriteria.setQuery(sparql);
+//		log.info("SPARQL=" + sparql);
+//		queryCriteria.addNamedModel(appInfo.getMetarepositoryDataset());
+//		//RdfTable resultTable = Queryer.selectRdfTable(queryCriteria);
+//		List<String> datasetIds = Queryer.selectSimpleList(queryCriteria, "datasetId");
+		
+		List<String> datasetIds = new ArrayList<String>();
+		Collection<?> allExternalDatasets = persister.reconstituteAll(ExternalDataset.class);
+		if (allExternalDatasets == null || allExternalDatasets.size()==0) {
+			return returnQueryCriteria;
+		}
+		for (Object externalDatasetObj: allExternalDatasets) {
+			ExternalDataset externalDataset = (ExternalDataset)externalDatasetObj;
+			Collection<String> datasetFunctions = externalDataset.getDatasetFunctions();
+			if (datasetFunctions != null && datasetFunctions.contains(datasetFunctionId)) {
+				datasetIds.add(externalDataset.getId());
+			}
+		}
 		
 		returnQueryCriteria.addNamedModelIds(datasetIds);
-		returnQueryCriteria.setTextIndex(persister.getIndex(datasetFunctionId));
+		
 		log.info("Returning QueryCriteria for function " + datasetFunctionId + ", with these models:" + datasetIds);
 		return returnQueryCriteria;
 	}
 
-	private static String getSparqlToFindDatasetsOfFunction(String datasetFunctionId) {
-		String sparql = " PREFIX inqle: <" + RDF.INQLE + "> \n " + 
-		" PREFIX xsd: <" + RDF.XSD + "> \n " + 
-		" SELECT ?datasetId \n " +
-		" { \n " +
-		" GRAPH ?g { \n " +
-		" ?datasetUri a inqle:ExternalDataset \n " +
-		" . ?datasetUri inqle:datasetFunctions \"" + datasetFunctionId + "\"^^xsd:string \n" +
-		" } }\n";
-		return sparql;
-	}
+//	private static String getSparqlToFindDatasetsOfFunction(String datasetFunctionId) {
+//		String sparql = " PREFIX inqle: <" + RDF.INQLE + "> \n " + 
+//		" PREFIX xsd: <" + RDF.XSD + "> \n " + 
+//		" SELECT ?datasetId \n " +
+//		" { \n " +
+//		" GRAPH ?g { \n " +
+//		" ?datasetUri a inqle:ExternalDataset \n " +
+////		" . ?datasetUri inqle:datasetFunctions \"" + datasetFunctionId + "\"^^xsd:string \n" +
+//		" } }\n";
+//		return sparql;
+//	}
 }
