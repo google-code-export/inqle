b48b75fd73d8bc1c3ad86d3be9d88ff0aeb3bbc9
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2008-10-14 02:52:25 +0000

    significant rework of Arc, ArcSet, ArcTable added code for importing data from RDF into ArcTable

--- trunk/org.inqle.experiment.rapidminer/src/main/org/inqle/experiment/rapidminer/util/ModelConverter.java
+++ trunk/org.inqle.experiment.rapidminer/src/main/org/inqle/experiment/rapidminer/util/ModelConverter.java
@@ -1,18 +1,30 @@
 package org.inqle.experiment.rapidminer.util;
 
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
 
-import org.inqle.data.rdf.jena.util.ArcSetExtractor;
+import org.apache.log4j.Logger;
+import org.inqle.data.rdf.jena.util.ArcTable;
+import org.inqle.data.rdf.jena.util.ArcTableFactory;
+import org.inqle.data.rdf.jena.util.Converter;
+import org.inqle.data.rdf.jenabean.Arc;
 import org.inqle.data.rdf.jenabean.ArcSet;
+import org.inqle.data.sampling.DataCell;
+import org.inqle.data.sampling.DataColumn;
 
 import com.hp.hpl.jena.ontology.OntModel;
 import com.hp.hpl.jena.rdf.model.Resource;
 import com.rapidminer.example.Attribute;
+import com.rapidminer.example.table.AttributeFactory;
+import com.rapidminer.example.table.DoubleArrayDataRow;
 import com.rapidminer.example.table.MemoryExampleTable;
+import com.rapidminer.example.table.NominalMapping;
+import com.rapidminer.example.table.PolynominalMapping;
+import com.rapidminer.tools.Ontology;
 
 /**
- * Converts Jena Model into RapidMiner ExampleTable
+ * Converts Jena Model into RapidMiner MemoryExampleTable
  * @author David Donohue
  * Oct 10, 2008
  */
@@ -21,24 +33,106 @@ public class ModelConverter {
 	private OntModel ontModel;
 	private Resource subjectClass;
 
+	private static Logger log = Logger.getLogger(ModelConverter.class);
+	
 	public ModelConverter(OntModel ontModel, Resource subjectClass) {
 		this.ontModel = ontModel;
 		this.subjectClass = subjectClass;
 	}
 	
 	public MemoryExampleTable createExampleTable() {
-		List<Attribute> attributes = new ArrayList<Attribute>();
-		ArcSetExtractor arcSetExtractor = new ArcSetExtractor(ontModel);
-		List<ArcSet> arcSets = arcSetExtractor.extract(subjectClass);
 		
-		//we now have an ArcSet for each row.  Each ArcSet contains 1 Arc for each column (or 0 when the column
-		//has missing value for the row)
+		ArcTableFactory arcTableFactory = new ArcTableFactory(ontModel);
+		ArcTable arcTable = arcTableFactory.createArcTable(subjectClass);
 		
+		List<Attribute> attributes = getAttributes(arcTable);
 		
 		MemoryExampleTable table = new MemoryExampleTable(attributes);
 		
 		
+		
+		
+		//loop through rows of the DataTable, filling the table
+		List<List<Object>> rows = arcTable.getRows();
+		for (List<Object> row: rows) {//loop thru each row
+//		for (int rowIndex = 0; rowIndex < dataTable.getRows().size(); rowIndex++) {//loop thru each row
+			double[] data = new double[attributes.size()];
+
+			//loop thru each column
+			for (int colIndex = 0; colIndex < attributes.size(); colIndex++) {
+				Attribute attribute = attributes.get(colIndex);
+				
+				Object cell = row.get(colIndex);
+
+				double val = 0;
+				if (attribute.getValueType() == Ontology.REAL) {//Numeric
+					try {
+						val = Double.parseDouble(cell.toString());
+					} catch (Exception e) {
+						log.error("Unable to cast datum '" + cell.toString() + "' as a number.", e);
+					}
+				} else if (attribute.getValueType() == Ontology.SINGLE_VALUE) {
+					//do not add anything?
+				} else {//Nominal or String
+//					log.trace("Get mapping for non-numeric attribute: " + attribute + "; ALL VALUES=" + arcTable.getColumnValues(columnIndex));
+					NominalMapping mapping = attribute.getMapping();
+					//val = column.getValuesSet().indexOf(cell.toString());
+					val = mapping.mapString(cell.toString());
+					if (attribute.getValueType() == Ontology.POLYNOMINAL) {
+						log.info("Cell val '" + cell + "' maps to " + val);
+					}
+				}
+				//fill with proper data here
+				data[colIndex] = val;
+			}//loop back to next column
+			
+			//add data row
+			table.addDataRow(new DoubleArrayDataRow(data));
+		}//loop back to next row
+		
+		
+		
+		
 		return table;
+	}
+	
+	private List<Attribute> getAttributes(ArcTable arcTable) {
+		List<Attribute> attributes = new ArrayList<Attribute>();
+		int columnCount = arcTable.getHeaders().size();
+		for (int i=0; i<columnCount; i++) {
+			int dataType = arcTable.getDataType(i);
+			int columnType = arcTable.getColumnType(i);
+			int rapidMinerDataType = Ontology.POLYNOMINAL;
+			if (columnType==ArcTable.COLUMN_TYPE_NO_VALUES || dataType==ArcTable.DATA_TYPE_NO_VALUES) {
+				rapidMinerDataType = Ontology.SINGLE_VALUE;
+			} else if (columnType==ArcTable.COLUMN_TYPE_ONE_VALUE) {
+				rapidMinerDataType = Ontology.SINGLE_VALUE;
+			} else if (columnType==ArcTable.COLUMN_TYPE_ALL_UNIQUE && dataType==ArcTable.DATA_TYPE_STRING) {
+				rapidMinerDataType = Ontology.STRING;
+			} else if (dataType==ArcTable.DATA_TYPE_DATE) {
+				rapidMinerDataType = Ontology.DATE_TIME;
+			} else if(dataType==ArcTable.DATA_TYPE_NUMERIC) {
+				rapidMinerDataType = Ontology.REAL;
+			}
+			
+			if (rapidMinerDataType != Ontology.REAL) {
+				PolynominalMapping mapping = new PolynominalMapping();
+				LinkedHashSet<Object> valuesSet = arcTable.getColumnDistinctValues(i);
+				int j=0;
+				for (Object strObj: valuesSet) {
+					String strVal = strObj.toString();
+					log.debug("Mapping " + i + ": " + strVal);
+					mapping.mapString(strVal.trim());
+					j++;
+				}
+			}
+			
+			
+			Arc headerArc = arcTable.getHeaderList().get(i);
+			Attribute anAttribute = AttributeFactory.createAttribute(headerArc.toString(), rapidMinerDataType);
+			attributes.add(anAttribute);
+		}
+		return attributes;
 	}
 
 }
