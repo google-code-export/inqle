e6bed7d5c143fa909423997c100040bdb3f212e8
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2008-10-26 11:28:26 +0000

    changed to -server JVM

--- trunk/InqleFeature/dist_root/run-inqle-console.bat
+++ trunk/InqleFeature/dist_root/run-inqle-console.bat
@@ -1,1 +1,1 @@
-eclipse -console -consolelog -debug -dev -Xmx512m
+eclipse -server -console -consolelog -debug -dev -Xmx512m
--- trunk/InqleFeature/dist_root/run-inqle-console.sh
+++ trunk/InqleFeature/dist_root/run-inqle-console.sh
@@ -1,6 +1,6 @@
 echo starting INQLE...
 echo ----------------------------
-java -Dorg.osgi.framework.bootdelegation=* -Xmx512m -jar org.eclipse.osgi_3.3.2.R33x_v20080105.jar -configuration config-linux -consoleLog -debug -clean -console -dev
+java -Dorg.osgi.framework.bootdelegation=* -server -Xmx512m -jar org.eclipse.osgi_3.3.2.R33x_v20080105.jar -configuration config-linux -consoleLog -debug -clean -console -dev
 echo -----------------------------
 echo INQLE stopped
 #org.eclipse.equinox.launcher_1.0.1.R33x_v20070828.jar
