43bc7709e9c42e5dc311b022468f435ba800bbf8
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2008-10-30 21:38:13 +0000

    fixed bugs in file data wizard sampler wizard almost working

--- trunk/org.inqle.http.lookup/src/main/org/inqle/http/lookup/SubjectLookup.java
+++ trunk/org.inqle.http.lookup/src/main/org/inqle/http/lookup/SubjectLookup.java
@@ -4,6 +4,7 @@ import java.util.Iterator;
 
 import org.apache.log4j.Logger;
 import org.inqle.core.util.InqleInfo;
+import org.inqle.data.rdf.Data;
 import org.inqle.data.rdf.RDF;
 import org.inqle.data.rdf.jena.QueryCriteria;
 import org.inqle.data.rdf.jena.QueryCriteriaFactory;
@@ -55,6 +56,38 @@ public class SubjectLookup {
 			return sparql;
 		}
 
+	/**
+	 * Generate SPARQL for finding subclasses of owlClassUri,
+	 * matching the searchTerm.  If owlClassUri is null, search all classes
+	 * @param searchTerm
+	 * @param superClassUri
+	 * @param limit
+	 * @param offset
+	 * @return
+	 */
+	public static String getSparqlSearchRdfsAndSpecifiedSubclasses(String searchTerm, String superClassUri, int limit, int offset) {
+			String sparql = 
+				"PREFIX rdf: <" + RDF.RDF + ">\n" + 
+				"PREFIX rdfs: <" + RDF.RDFS + ">\n" + 
+				"PREFIX owl: <" + RDF.OWL + ">\n" + 
+				"PREFIX pf: <" + RDF.PF + ">\n" + 
+				"PREFIX inqle: <" + RDF.INQLE + ">\n" + 
+				"SELECT DISTINCT ?URI ?Label ?Comment \n" +
+				"{\n" +
+				"GRAPH ?g {\n" +
+				"(?URI ?Score) pf:textMatch ( '" + searchTerm + "' " + MINIMUM_SCORE_THRESHOLD + " ) \n" +
+				". { \n" +
+				"{ ?URI rdfs:subClassOf <" + superClassUri + "> } \n" +
+				"UNION \n" +
+				"{ ?URI rdfs:subClassOf rdfs:Class } \n" +
+				"} \n" +
+				". OPTIONAL { ?URI rdfs:label ?Label }\n" +
+				". OPTIONAL { ?URI rdfs:comment ?Comment } \n" +
+				"} } ORDER BY DESC(?Score) \n" +
+				"LIMIT " + limit + " OFFSET " + offset;
+			return sparql;
+		}
+	
 	/**
 	 * This is hardcoded to query on the default graph (model) only
 	 * TODO handle named graphs for this query
@@ -148,6 +181,25 @@ public class SubjectLookup {
 	}
 	
 	/**
+	 * Lookup subclasses of inqle:Subject in the rg.inqle.datasets.dataSubject
+	 * internal dataset
+	 * @param searchTermForRdfClass
+	 * @param countSearchResults
+	 * @param offset
+	 * @return
+	 */
+	public static String lookupSubjectsInSubjectsDataset (
+		String searchTermForRdfClass, 
+		int countSearchResults, 
+		int offset) {
+		return lookupSubclassesInInternalDataset(
+				searchTermForRdfClass,
+				RDF.SUBJECT,
+				Data.DATA_SUBJECT_DATASET_ROLE_ID, 
+				countSearchResults, 
+				offset);
+	}
+	/**
 	 *  Lookup in the specified internal dataset any resource, of the provided OWL class URI, which matches the provided search term.
 	 * @param searchTermForRdfClass
 	 * @param superClassUri the OWL superclass of the subjects to find.  Leave null to search RDFS classes (i.e. subclasses of rdfs:Class)
@@ -167,22 +219,61 @@ public class SubjectLookup {
 		queryCriteria.addNamedModel(persister.getInternalDataset(internalDatasetRoleId));
 		IndexLARQ textIndex =  persister.getIndex(internalDatasetRoleId);
 		Iterator<?> searchResultI = textIndex.search(searchTermForRdfClass);
-		log.trace("Searched " + internalDatasetRoleId + " index for '" + searchTermForRdfClass + "'...");
+		log.info("Searched " + internalDatasetRoleId + " index for '" + searchTermForRdfClass + "'...");
 		while(searchResultI.hasNext()) {
 			HitLARQ hit = (HitLARQ)searchResultI.next();
-			log.trace("Found result: " + hit.getNode() + "; score=" + hit.getScore());
+			log.info("Found result: " + hit.getNode() + "; score=" + hit.getScore());
 		}
 		if (textIndex != null) {
 			queryCriteria.setTextIndex(textIndex);
 		}
 		String sparql = getSparqlSearchRdfSubclasses(searchTermForRdfClass, superClassUri, countSearchResults, offset);
-		log.trace("Querying w/ this sparql:\n" + sparql);
+		log.info("Querying w/ this sparql:\n" + sparql);
 		queryCriteria.setQuery(sparql);
 		String matchingClassesXml = Queryer.selectXml(queryCriteria);
-		//log.info("Queried and got these matching results:\n" + matchingClassesXml);
+		log.info("lookupSubclassesInInternalDataset(): Queried and got these matching results:\n" + matchingClassesXml);
 		return matchingClassesXml;
 	}
 
+	/**
+	 * Search the specified internal dataset for subclasses of rdfs:Class UNION 
+	 * the specified class URI
+	 * 
+	 * @param searchTermForRdfClass
+	 * @param superClassUri
+	 * @param internalDatasetRoleId
+	 * @param countSearchResults
+	 * @param offset
+	 * @return
+	 */
+	public static String lookupRdfsAndSpecifiedSubclassesInInternalDataset (
+			String searchTermForRdfClass, 
+			String superClassUri, 
+			String internalDatasetRoleId, 
+			int countSearchResults, 
+			int offset) {
+		Persister persister = Persister.getInstance();
+		QueryCriteria queryCriteria = new QueryCriteria();
+		queryCriteria.addNamedModel(persister.getInternalDataset(internalDatasetRoleId));
+		IndexLARQ textIndex =  persister.getIndex(internalDatasetRoleId);
+		Iterator<?> searchResultI = textIndex.search(searchTermForRdfClass);
+		log.info("Searched " + internalDatasetRoleId + " index for '" + searchTermForRdfClass + "'...");
+		while(searchResultI.hasNext()) {
+			HitLARQ hit = (HitLARQ)searchResultI.next();
+			log.info("Found result: " + hit.getNode() + "; score=" + hit.getScore());
+		}
+		if (textIndex != null) {
+			queryCriteria.setTextIndex(textIndex);
+		}
+		String sparql = getSparqlSearchRdfsAndSpecifiedSubclasses(searchTermForRdfClass, superClassUri, countSearchResults, offset);
+		log.info("Querying w/ this sparql:\n" + sparql);
+		queryCriteria.setQuery(sparql);
+		String matchingClassesXml = Queryer.selectXml(queryCriteria);
+		log.info("lookupSubclassesInInternalDataset(): Queried and got these matching results:\n" + matchingClassesXml);
+		return matchingClassesXml;
+	}
+
+	
 	/**
 	 * Lookup any resource, of the provided OWL class URI, which matches the provided search term.
 	 * @param searchTermForRdfClass the user-entered query term
@@ -234,29 +325,29 @@ public class SubjectLookup {
 		QueryCriteria queryCriteria = QueryCriteriaFactory.createQueryCriteriaForDatasetFunction(Persister.EXTENSION_DATASET_FUNCTION_SCHEMAS);
 		//add any internal RDF schemas
 //		DatafileUtil.addDatafiles(queryCriteria, InqleInfo.getRdfSchemaFilesDirectory());
-		log.info("Get/Create index of Model...");
+		log.trace("Get/Create index of Model...");
 //		IndexLARQ textIndex =  persister.getSchemaFilesSubjectIndex();
 		IndexLARQ textIndex =  persister.getIndex(Persister.EXTENSION_DATASET_FUNCTION_SCHEMAS);
 		if (textIndex==null) return null;
 		
 		Iterator<?> searchResultI = textIndex.search(searchTermForRdfClass);
-		log.info("Searched Schema Datasets index for '" + searchTermForRdfClass + "'...");
+		log.trace("Searched Schema Datasets index for '" + searchTermForRdfClass + "'...");
 		while(searchResultI.hasNext()) {
 			HitLARQ hit = (HitLARQ)searchResultI.next();
-			log.info("Found result: " + hit.getNode() + "; score=" + hit.getScore());
+			log.trace("Found result: " + hit.getNode() + "; score=" + hit.getScore());
 		}
 		if (textIndex != null) {
 			queryCriteria.setTextIndex(textIndex);
 		}
 		
-		log.info("Get/Create OntModel...");
+		log.trace("Get/Create OntModel...");
 //		queryCriteria.setSingleModel(persister.getSchemaFilesOntModel());
 		
 		String sparql = getSparqlSearchSchemaDatasetsForSubjects(searchTermForRdfClass, countSearchResults, offset);
-		log.info("Querying w/ this sparql:\n" + sparql);
+		log.trace("Querying w/ this sparql:\n" + sparql);
 		queryCriteria.setQuery(sparql);
 		String matchingClassesXml = Queryer.selectXml(queryCriteria);
-		log.info("Queried Schema Datasets and got these matching results:\n" + matchingClassesXml);
+		log.trace("Queried Schema Datasets and got these matching results:\n" + matchingClassesXml);
 		return matchingClassesXml;
 	}
 	
@@ -274,16 +365,16 @@ public class SubjectLookup {
 		QueryCriteria queryCriteria = QueryCriteriaFactory.createQueryCriteriaForDatasetFunction(Persister.EXTENSION_DATASET_FUNCTION_SCHEMAS);
 		//add any internal RDF schemas
 //		DatafileUtil.addDatafiles(queryCriteria, InqleInfo.getRdfSchemaFilesDirectory());
-		log.info("Get/Create index of Model...");
+		log.trace("Get/Create index of Model...");
 //		IndexLARQ textIndex =  persister.getSchemaFilesSubjectIndex();
 		IndexLARQ textIndex =  persister.getIndex(Persister.EXTENSION_DATASET_FUNCTION_SCHEMAS);
 		if (textIndex==null) return null;
 		
 		Iterator<?> searchResultI = textIndex.search(searchTermForRdfClass);
-		log.info("Searched Schema Datasets index for '" + searchTermForRdfClass + "'...");
+		log.trace("Searched Schema Datasets index for '" + searchTermForRdfClass + "'...");
 		while(searchResultI.hasNext()) {
 			HitLARQ hit = (HitLARQ)searchResultI.next();
-			log.info("Found result: " + hit.getNode() + "; score=" + hit.getScore());
+			log.trace("Found result: " + hit.getNode() + "; score=" + hit.getScore());
 		}
 		if (textIndex != null) {
 			queryCriteria.setTextIndex(textIndex);
