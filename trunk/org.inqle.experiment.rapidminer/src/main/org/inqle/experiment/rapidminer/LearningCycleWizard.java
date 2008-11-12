03ae6789f1f0086a9e8d75827d55d6288dd51f55
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2008-11-12 11:23:02 +0000

    added QNames to SimpleSubjectSamplerWizard

--- trunk/org.inqle.experiment.rapidminer/src/main/org/inqle/experiment/rapidminer/LearningCycleWizard.java
+++ trunk/org.inqle.experiment.rapidminer/src/main/org/inqle/experiment/rapidminer/LearningCycleWizard.java
@@ -3,16 +3,23 @@
  */
 package org.inqle.experiment.rapidminer;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.apache.log4j.Logger;
+import org.eclipse.jface.wizard.IWizardPage;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Shell;
 import org.inqle.data.sampling.ISampler;
 import org.inqle.data.sampling.SamplerLister;
+import org.inqle.ui.rap.IList2Provider;
+import org.inqle.ui.rap.IListProvider;
+import org.inqle.ui.rap.IValueUpdater;
 import org.inqle.ui.rap.actions.DynaWizard;
 import org.inqle.ui.rap.pages.NameDescriptionPage;
 import org.inqle.ui.rap.pages.RadioOrListSelectorPage;
+import org.inqle.ui.rap.pages.SimpleListSelectorPage;
 
 import com.hp.hpl.jena.rdf.model.Model;
 
@@ -20,16 +27,22 @@ import com.hp.hpl.jena.rdf.model.Model;
  * @author David Donohue
  * Apr 15, 2008
  */
-public class LearningCycleWizard extends DynaWizard {
+public class LearningCycleWizard extends DynaWizard implements IListProvider, IList2Provider, IValueUpdater {
 
 	//private LearningCycle learningCycle;
 
-	private static final String[] RADIO_OPTIONS = {
-		"Use a randomly-selected Sampler",
-		"Use the below-selected Sampler"
-	};
+//	private static final String[] RADIO_OPTIONS = {
+//		"Use a randomly-selected Sampler",
+//		"Use the below-selected Sampler"
+//	};
+
+	private static final String OPTION_USE_RANDOM_SAMPLER = "(Randomly select a sampler each cycle)";
 	
 	private static Logger log = Logger.getLogger(LearningCycleWizard.class);
+
+	private SimpleListSelectorPage samplerSelectorPage;
+
+	private List<ISampler> allSamplers;
 	
 	public LearningCycleWizard(LearningCycle learningCycle, Model learningCycleModel, Shell shell) {
 		super(learningCycleModel, shell);
@@ -46,39 +59,60 @@ public class LearningCycleWizard extends DynaWizard {
 		NameDescriptionPage nameDescriptionPage = new NameDescriptionPage(learningCycle, "Name and Description", null);
 		addPage(nameDescriptionPage);
 		
-//		ListSelectorPage samplerSelectorPage = new ListSelectorPage(learningCycle, "sampler", "Select sampler to use", null);
-//		samplerSelectorPage.setBeanItemClass(ISampler.class);
-//		ISampler[] nullSamplerArray = {};
-//		samplerSelectorPage.setListItems(SamplerLister.listSamplers().toArray(nullSamplerArray));
-//		addPage(samplerSelectorPage);
-		
-		RadioOrListSelectorPage samplerSelectorPage = new RadioOrListSelectorPage(bean, "sampler", "Select Sampler to use", null);
-		samplerSelectorPage.setBeanItemClass(ILearningCycle.class);
-		ISampler[] nullSamplerArray = {};
-		samplerSelectorPage.setListItems(SamplerLister.listSamplers().toArray(nullSamplerArray));
-		List<String> radioOptionTexts = Arrays.asList(RADIO_OPTIONS);
-		samplerSelectorPage.setRadioOptionTexts(radioOptionTexts);
-		SamplerBeanProvider samplerBeanProvider = new SamplerBeanProvider();
-		samplerBeanProvider.setBean(bean);
-		samplerSelectorPage.setRadioBeanProvider(samplerBeanProvider);
+		samplerSelectorPage = new SimpleListSelectorPage("Select sampler to use", "Select whether to use a randomly selected sampler, or specify the sampler to be used for this learning cycle.", "Select sampler:", SWT.SINGLE);
 		addPage(samplerSelectorPage);
 		
 		SamplingResultPage samplingResultsPage = new SamplingResultPage(learningCycle, "Result of Sampling");
-		//samplingResultsPage.setPersister(persister);
 		addPage(samplingResultsPage);
 		
 //		LabelSelectorPage labelSelectorPage = new LabelSelectorPage(learningCycle, "labelDataColumn", "Select column to be used as the label", null);
 //		labelSelectorPage.setDataTableProvider(samplingResultsPage);
 //		addPage(labelSelectorPage);
 	}
-
-	/* (non-Javadoc)
-	 * @see org.inqle.ui.rap.actions.DynaWizard#getBean()
+	
+	/**
+	 * This is called by some wizard pages, to retrieve the list of items to display
 	 */
-//	@Override
-//	public Object getBean() {
-//		// TODO Auto-generated method stub
-//		return this.learningCycle;
-//	}
+	@SuppressWarnings("unchecked")
+	public List getList(IWizardPage page) {
+		if (bean == null) return null;
+		if (page.equals(samplerSelectorPage)) {
+			List<String> listItems = new ArrayList<String>();
+			listItems.add(OPTION_USE_RANDOM_SAMPLER);
+			if (allSamplers==null) {
+				allSamplers = SamplerLister.listSamplers();
+			}
+			for (ISampler sampler: allSamplers) {
+				listItems.add(sampler.getName());
+			}
+			return listItems;
+		}
+		
+		return null;
+	}
 
+	public List<Object> getList2(IWizardPage page) {
+		if (bean == null) return null;
+		LearningCycle learningCycle = (LearningCycle) bean;
+		if (page.equals(samplerSelectorPage)) {
+			List<Object> listItems = new ArrayList<Object>();
+			listItems.add(learningCycle.getSampler().getName());
+			return listItems;
+		}
+		
+		return null;
+	}
+
+	public void updateValue(IWizardPage page) {
+		if (bean == null) return;
+		LearningCycle learningCycle = (LearningCycle) bean;
+		if (page.equals(samplerSelectorPage)) {
+			int selectedSamplerIndex = samplerSelectorPage.getSelectedIndex();
+			if (selectedSamplerIndex < 1) {
+				learningCycle.setSampler(null);
+			} else {
+				learningCycle.setSampler(allSamplers.get(selectedSamplerIndex - 1));
+			}
+		}
+	}
 }
