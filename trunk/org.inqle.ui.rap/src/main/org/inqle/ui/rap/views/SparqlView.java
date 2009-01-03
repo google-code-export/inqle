aeaac1ac2be4d305e2b769a20eae667c8d05a473
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2009-01-03 03:56:54 +0000

    renamed ~2 to ~

--- trunk/org.inqle.ui.rap/src/main/org/inqle/ui/rap/views/SparqlView.java
+++ trunk/org.inqle.ui.rap/src/main/org/inqle/ui/rap/views/SparqlView.java
@@ -1,12 +1,9 @@
 package org.inqle.ui.rap.views;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
-import org.eclipse.core.databinding.observable.list.WritableList;
-import org.eclipse.jface.databinding.viewers.ObservableListContentProvider;
-import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
@@ -14,33 +11,35 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Listener;
-import org.eclipse.swt.widgets.Table;
-import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.Link;
 import org.eclipse.ui.part.ViewPart;
+import org.inqle.data.rdf.jena.NamedModel;
 import org.inqle.data.rdf.jena.QueryCriteria;
-import org.inqle.data.rdf.jena.RdfTable;
 import org.inqle.data.rdf.jena.sdb.Queryer;
 import org.inqle.data.rdf.jenabean.Persister;
-import org.inqle.ui.rap.table.QuerySolutionTableLabelProvider;
+import org.inqle.ui.rap.widgets.ResultSetTable;
 
-import com.hp.hpl.jena.query.QuerySolution;
+import com.hp.hpl.jena.query.ResultSetRewindable;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.Resource;
 
 /**
  * @author David Donohue
  * May 6, 2008
  */
-public abstract class SparqlView extends ViewPart implements SelectionListener, Listener {
+public abstract class SparqlView extends ViewPart implements SelectionListener {
 
 	private static final Logger log = Logger.getLogger(SparqlView.class);
 	
-	private static final int COLUMN_WIDTH = 120;
+//	private static final int COLUMN_WIDTH = 120;
 
 	private static final int DEFAULT_RECORD_COUNT_INDEX = 0;
-	private static final String[] RECORD_COUNT_OPTIONS = {"10", "20", "50", "100", "200", "500", "1000", "5000", "10000" };
+	private static final String[] RECORD_COUNT_OPTIONS = {"10", "20", "50", "100", "200", "500", "1000" };
 	private static final String DESC = "DESC";
 	private static final String ASC = "ASC";
 
@@ -48,25 +47,31 @@ public abstract class SparqlView extends ViewPart implements SelectionListener, 
 	
 	private Composite composite;
 	
-	protected List<QuerySolution> rows = new ArrayList<QuerySolution>();
-	
-	/**
-	 * the class of items in the table's List field.
-	 */
-	protected Class<?> tableBeanClass = QuerySolution.class;
+//	protected List<QuerySolution> rows = new ArrayList<QuerySolution>();
 
 	//protected Persister persister;
-	protected TableViewer tableViewer;
+	protected ResultSetTable resultSetTable;
 	protected List<String> propertyNames;
 	protected Button refreshButton;
 	protected String currentSortColumn = "Creation_Date";
 	protected String currentSortDirection = DEFAULT_SORT_DIRECTION;
-	protected org.eclipse.swt.widgets.List recordCountList;
+	protected Combo recordCountCombo;
 	protected Button previousButton;
 	protected Button nextButton;
 	protected Label resultDescription;
 	
 	private int offset = 0;
+
+	private ResultSetRewindable resultSet;
+
+	private Button deleteButton;
+
+	private NamedModel namedModel;
+
+	private Button checkAllButton;
+
+	private Button uncheckAllButton;
+	
 	/* (non-Javadoc)
 	 * @see org.eclipse.ui.part.WorkbenchPart#createPartControl(org.eclipse.swt.widgets.Composite)
 	 */
@@ -83,8 +88,9 @@ public abstract class SparqlView extends ViewPart implements SelectionListener, 
     
 		showForm();
 		showResultDescription();
-		showTable();
-		refreshForm();
+//		showTable();
+//		refreshForm();
+		refreshView();
 	}
 
 	private void showForm() {
@@ -93,9 +99,10 @@ public abstract class SparqlView extends ViewPart implements SelectionListener, 
 		formComposite.setLayout(rowLayout);
 		
 		//select list for number of records to show
-		recordCountList = new org.eclipse.swt.widgets.List(formComposite, SWT.BORDER | SWT.SINGLE | SWT.V_SCROLL);
-		recordCountList.setItems(RECORD_COUNT_OPTIONS);
-		recordCountList.setSelection(DEFAULT_RECORD_COUNT_INDEX);
+		new Label(formComposite, SWT.NONE).setText("Number of rows");
+		recordCountCombo = new Combo(formComposite, SWT.BORDER | SWT.READ_ONLY);
+		recordCountCombo.setItems(RECORD_COUNT_OPTIONS);
+		recordCountCombo.select(DEFAULT_RECORD_COUNT_INDEX);
 		//GridData gridData = new GridData(GridData.HORIZONTAL_ALIGN_FILL | GridData.GRAB_HORIZONTAL);
 		//formComposite.setLayoutData(gridData);
 		refreshButton = new Button(formComposite, SWT.PUSH);
@@ -114,43 +121,46 @@ public abstract class SparqlView extends ViewPart implements SelectionListener, 
 		previousButton = new Button(descriptionComposite, SWT.PUSH);
 		previousButton.setText("<--");
 		previousButton.addSelectionListener(this);
+		previousButton.setEnabled(false);
 		nextButton = new Button(descriptionComposite, SWT.PUSH);
 		nextButton.setText("-->");
 		nextButton.addSelectionListener(this);
+		nextButton.setEnabled(false);
+		checkAllButton = new Button(descriptionComposite, SWT.PUSH);
+		checkAllButton.setText("Check All");
+		checkAllButton.addSelectionListener(this);
+		checkAllButton.setEnabled(false);
+		uncheckAllButton = new Button(descriptionComposite, SWT.PUSH);
+		uncheckAllButton.setText("Uncheck All");
+		uncheckAllButton.addSelectionListener(this);
+		uncheckAllButton.setEnabled(false);
+		deleteButton = new Button(descriptionComposite, SWT.PUSH);
+		deleteButton.setText("Delete");
+		deleteButton.addSelectionListener(this);
+		deleteButton.setEnabled(false);
 	}
 	
 	public void createTable() {
-		//if no data, return
-		if (rows == null || rows.size() == 0) {
+		resultSetTable = new ResultSetTable(composite, SWT.MULTI, resultSet);
+		resultSetTable.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		resultSetTable.renderTable(this);
+		if (resultSet == null || resultSet.size() == 0) {
 			return;
 		}
-		
-		Table table = new Table(composite, SWT.NONE);
-		
-		table.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-		
-		table.setHeaderVisible(true);
-		table.setLinesVisible(true);
-
-		//add columns
-		for (String propertyName: getPropertyNames()) {
-			TableColumn column = new TableColumn(table,SWT.LEFT);
-			if (propertyName==null) propertyName="";
-			column.setText(propertyName.replaceAll("_", " "));
-			column.setToolTipText("Click to sort");
-			column.setResizable(true);
-			column.setWidth(getColumnWidth());
-			column.addListener(SWT.Selection, this);
-			log.trace("Added column: " + propertyName);
-		}
-		
-		tableViewer = new TableViewer(table);
-		ObservableListContentProvider olContentProvider = new ObservableListContentProvider();
-		tableViewer.setContentProvider(olContentProvider);
-		
-		QuerySolutionTableLabelProvider labelProvider = new QuerySolutionTableLabelProvider();
-		labelProvider.setColumnFields(getPropertyNames());
-		tableViewer.setLabelProvider(labelProvider);
+		log.info("Rendered table for result set of size: " + resultSet.size());
+	}
+	
+	public void recreateTable() {
+		resultSetTable.dispose();
+		createTable();
+		resultSetTable.redraw();
+//		resultSetTable.clearTable();
+//		resultSetTable.setResultSet(resultSet);
+//		resultSetTable.renderTable(this);
+//		if (resultSet == null || resultSet.size() == 0) {
+//			return;
+//		}
+//		log.info("Rendered table for result set of size: " + resultSet.size());
 	}
 	
 	/**
@@ -158,69 +168,51 @@ public abstract class SparqlView extends ViewPart implements SelectionListener, 
 	 * By default, set to 100
 	 * @return
 	 */
-	private int getColumnWidth() {
-		return COLUMN_WIDTH;
-	}
+//	private int getColumnWidth() {
+//		return COLUMN_WIDTH;
+//	}
 
 	public void showTable() {
-		if (rows == null || rows.size() == 0) {
-			doQuery();
-		}
-		
-		if (tableViewer == null) {
-			//if no data, do not create the table
-			if (rows == null || rows.size() == 0) {
-				return;
-			}
-			//otherwise, do create the table
+//		if (resultSet == null || resultSet.size() == 0) {
+//			doQuery();
+//		}
+//		
+//		if (resultSet == null || resultSet.size() == 0) {
+//			return;
+//		}
+//		
+		if (resultSetTable == null) {
 			createTable();
+		} else {
+			recreateTable();
 		}
 		//update the results description
-		resultDescription.setText("Showing results " + (offset + 1) + " to " + (rows.size() + offset));
+		String message = "No items found.";
+		if (resultSet != null && resultSet.size() > 0) {
+			message = "Showing results " + (offset + 1) + " to " + (resultSet.size() + offset);
+		}
+		resultDescription.setText(message);
 		
-		WritableList writableListInput = new WritableList(rows, tableBeanClass);
-		log.trace("writableListInput=" + writableListInput);
-		tableViewer.setInput(writableListInput);
-		//tableViewer.getTable().pack();
-		tableViewer.refresh(false);
-		//composite.getShell().pack();
+		
 	}
 	
 	@Override
 	public void setFocus() {
-		showTable();
-	}
-	
-	/**
-	 * Set the rows of data in the table.  The class of items should be set in the setRowItemClass() method
-	 * @param beans
-	 */
-	public void setRdfTable(RdfTable rdfTable) {
-		//this.rdfTable = rdfTable;
-		this.rows = rdfTable.getResultList();
-		if (getPropertyNames() == null) {
-			setPropertyNames(rdfTable.getVarNameList());
-		}
-	}
-
-	public List<QuerySolution> getRows() {
-		return rows;
+//		showTable();
 	}
 	
 	public void doQuery() {
-		Persister persister = Persister.getInstance();
 		String sparql = getSparql();
-		log.trace("Querying w/ SPARQL:" + sparql);
+		log.info("Querying w/ SPARQL:" + sparql);
 		QueryCriteria queryCriteria = new QueryCriteria();
 		queryCriteria.setQuery(sparql);
-		//TODO change to LogModel
-		queryCriteria.addNamedModel(persister.getInternalDataset(getDatasetRole()));
-		RdfTable resultTable = Queryer.selectRdfTable(queryCriteria);
-		log.trace("Received these results: " + resultTable.getResultList());
-		setRdfTable(resultTable);
+		queryCriteria.addNamedModel(getNamedModel());
+		this.resultSet = Queryer.selectResultSet(queryCriteria);
 	}
 
-	public abstract String getDatasetRole();
+	public NamedModel getNamedModel() {
+		return namedModel;
+	}
 
 	public abstract String getSparql();
 
@@ -236,7 +228,7 @@ public abstract class SparqlView extends ViewPart implements SelectionListener, 
 //	}
 	
 	public int getRecordCount() {
-		int selectedIndex = recordCountList.getSelectionIndex();
+		int selectedIndex = recordCountCombo.getSelectionIndex();
 		String recordCountStr = RECORD_COUNT_OPTIONS[selectedIndex];
 		int recordCount = Integer.parseInt(recordCountStr);
 		return recordCount;
@@ -250,10 +242,11 @@ public abstract class SparqlView extends ViewPart implements SelectionListener, 
 	 * SelectionListener method
 	 */
 	public void widgetSelected(SelectionEvent event) {
-		log.trace("event=" + event);
+		log.info("event=" + event);
 		if (event.getSource() == refreshButton) {
 			log.trace("Refresh clicked");
 			refreshView();
+			return;
 		}
 		
 		if (event.getSource() == previousButton) {
@@ -263,10 +256,11 @@ public abstract class SparqlView extends ViewPart implements SelectionListener, 
 			}
 			log.trace("Previous clicked");
 			refreshView();
+			return;
 		}
 		
 		if (event.getSource() == nextButton) {
-			if (rows.size() < getRecordCount()) {
+			if (resultSet.size() < getRecordCount()) {
 				refreshForm();
 				return;
 			}
@@ -274,16 +268,64 @@ public abstract class SparqlView extends ViewPart implements SelectionListener, 
 			
 			log.trace("Next clicked");
 			refreshView();
+			return;
+		}
+		
+		if (event.getSource() == checkAllButton) {
+//			log.info("Delete clicked");
+			checkAllItems(true);
+			return;
+		}
+		
+		if (event.getSource() == uncheckAllButton) {
+//		log.info("Delete clicked");
+		checkAllItems(false);
+		return;
+	}
+		
+		if (event.getSource() == deleteButton) {
+//			log.info("Delete clicked");
+			deleteSelectedItems();
+			return;
+		}
+		
+		if (event.getSource() instanceof Link) {
+			Link clickedLink = (Link)event.getSource();
+			Object linkData = clickedLink.getData();
+			if (linkData ==null) {
+				log.warn("Clicked link has no data.");
+				return;
+			}
+			String linkValue = linkData.toString();
+			//reset to the first record
+			offset = 0;
+			if (currentSortColumn.equals(linkValue)) {
+				toggleCurrentSortDirection();
+				
+			} else {
+				currentSortColumn = linkValue;
+				currentSortDirection = DEFAULT_SORT_DIRECTION;
+			}
+			log.info("Sort by " + currentSortColumn + " " + currentSortDirection);
+			//no need to refresh the form, since we are only changing sort
+			refreshView();
+			return;
 		}
 	}
 	
+	private void checkAllItems(boolean checked) {
+		resultSetTable.checkAllRows(checked);
+	}
+
 	private void refreshView() {
 		doQuery();
 		refreshForm();
 		showTable();
+		resultSetTable.redraw();
 	}
 
 	private void refreshForm() {
+		if (resultSet==null) return;
 		if (offset <= 0) {
 			log.trace("Disabled previousButton");
 			previousButton.setEnabled(false);
@@ -291,47 +333,30 @@ public abstract class SparqlView extends ViewPart implements SelectionListener, 
 			log.trace("Enabled previousButton");
 			previousButton.setEnabled(true);
 		}
-		if (rows.size() < getRecordCount()) {
+		if (resultSet.size() < getRecordCount()) {
 			log.trace("Disabled nextButton");
 			nextButton.setEnabled(false);
 		} else {
 			log.trace("Enabled nextButton");
 			nextButton.setEnabled(true);
 		}
-		
+		if (resultSet.size()>0) {
+			checkAllButton.setEnabled(true);
+			uncheckAllButton.setEnabled(true);
+			deleteButton.setEnabled(true);
+		} else {
+			checkAllButton.setEnabled(false);
+			uncheckAllButton.setEnabled(false);
+			deleteButton.setEnabled(false);
+		}
 	}
 
 	/**
 	 * SelectionListener method
 	 */
 	public void widgetDefaultSelected(SelectionEvent event) {
-		// do nothing
+		log.info("DefaultSelection: event=" + event);
 	}
-	
-	/**
-	 * Listener method (for clicking of column header)
-	 */
-	public void handleEvent(Event event) {
-		if (event.widget instanceof TableColumn) {
-			
-			TableColumn column = (TableColumn) event.widget;
-			String columnHeader = column.getText();
-			if (columnHeader==null) columnHeader="";
-			columnHeader = columnHeader.replaceAll(" ", "_");
-			//reset to the first record
-			offset = 0;
-			if (currentSortColumn.equals(columnHeader)) {
-				toggleCurrentSortDirection();
-			} else {
-				currentSortColumn = column.getText();
-				currentSortDirection = DEFAULT_SORT_DIRECTION;
-			}
-			log.trace("Sort by " + currentSortColumn + " " + currentSortDirection);
-			//no need to refresh the form, since we are only changing sort
-			doQuery();
-			showTable();
-		}
-  }
 
 	private void toggleCurrentSortDirection() {
 		if (currentSortDirection.equals(DESC)) {
@@ -343,10 +368,7 @@ public abstract class SparqlView extends ViewPart implements SelectionListener, 
 	}
 
 	public String getCurrentSortColumn() {
-		if (currentSortColumn == null) {
-			return null;
-		}
-		return currentSortColumn.replaceAll(" ", "_");
+		return currentSortColumn;
 	}
 
 	public void setCurrentSortColumn(String currentSortColumn) {
@@ -365,16 +387,58 @@ public abstract class SparqlView extends ViewPart implements SelectionListener, 
 		this.currentSortDirection = currentSortDirection;
 	}
 
-	/**
-	 * Override to specify the property names of your implementation.  This prevents the 
-	 * problem that optional fields might be missed in the first page
-	 * @return
-	 */
-	public List<String> getPropertyNames() {
-		return propertyNames;
+//	/**
+//	 * Override to specify the property names of your implementation.  This prevents the 
+//	 * problem that optional fields might be missed in the first page
+//	 * @return
+//	 */
+//	public List<String> getPropertyNames() {
+//		return propertyNames;
+//	}
+//
+//	public void setPropertyNames(List<String> propertyNames) {
+//		this.propertyNames = propertyNames;
+//	}
+	
+	public void deleteSelectedItems() {
+		List<String> checkedItems = resultSetTable.getCheckedItems();
+		if (checkedItems.size()==0) {
+			MessageDialog.openWarning(composite.getShell(), "Unable to Delete Items", "No items are selected below for deletion.  Please check boxes next to any idem(s) you would like to delete.");
+			return;
+		}
+		Persister persister = Persister.getInstance();
+		
+		boolean confirmDelete = MessageDialog.openConfirm(composite.getShell(), "Delete these items?", "Are you sure you want to delete these " + checkedItems.size() + " items?\nTHIS CANNOT BE UNDONE!\n" + checkedItems);
+		if (! confirmDelete) return;
+		
+		Model modelToDeleteFrom = persister.getModel(getNamedModel());
+		long sizeBeforeDelete = modelToDeleteFrom.size();
+		int deletedCount = 0;
+		for (String uriToDelete: checkedItems) {
+			Resource resourceToDelete = modelToDeleteFrom.getResource(uriToDelete);
+			if (resourceToDelete==null) {
+				log.info("Unable to retrieve resource from model: " + uriToDelete);
+				continue;
+			}
+			modelToDeleteFrom.removeAll(resourceToDelete, (Property)null, (RDFNode)null);
+			modelToDeleteFrom.removeAll((Resource)null, (Property)null, resourceToDelete);
+			deletedCount++;
+		}
+		long sizeAfterDelete = modelToDeleteFrom.size();
+		long totalDeletedStatements = sizeBeforeDelete - sizeAfterDelete;
+		if (deletedCount == checkedItems.size()) {
+			MessageDialog.openInformation(composite.getShell(), "Success Deleting", "Successfully deleted " + deletedCount + " items, " + totalDeletedStatements + " statements.");
+			refreshView();
+		} else if (deletedCount > 0) {
+			MessageDialog.openWarning(composite.getShell(), "Deleted Some Items", "Deleted " + deletedCount + " items, " + totalDeletedStatements + " statements.\nFailed to delete " + (checkedItems.size() - deletedCount)+ " items.");
+			refreshView();
+		} else {
+			MessageDialog.openWarning(composite.getShell(), "Failed to Delete Any Items", "No items were deleted.");
+		}
 	}
 
-	public void setPropertyNames(List<String> propertyNames) {
-		this.propertyNames = propertyNames;
+	public void setNamedModel(NamedModel namedModel) {
+		this.namedModel = namedModel;
 	}
+
 }
