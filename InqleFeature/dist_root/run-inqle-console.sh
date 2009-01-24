afc2d38977a7eff8ed12cfdd597fd5e359bb1945
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2009-01-24 12:45:32 +0000

    added -vmargs to enable argls like -server and -xmx256m to go to the JVM

--- InqleFeature/dist_root/run-inqle-console.sh
+++ InqleFeature/dist_root/run-inqle-console.sh
@@ -1,7 +1,6 @@
 echo starting INQLE...
 echo ----------------------------
-java -Dorg.osgi.framework.bootdelegation=* -server -Xmx128m -jar org.eclipse.osgi_3.3.2.R33x_v20080105.jar -configuration config-linux -consoleLog -debug -clean -console -dev
+java -Dorg.osgi.framework.bootdelegation=* -jar org.eclipse.osgi_3.3.2.R33x_v20080105.jar -configuration config-linux -consoleLog -debug -clean -console -dev -vmargs -server -Xmx256m -XX:+HeapDumpOnOutOfMemoryError
 echo -----------------------------
 echo INQLE stopped
-#org.eclipse.equinox.launcher_1.0.1.R33x_v20070828.jar
-#org.eclipse.osgi_3.3.2.R33x_v20080105.jar
+
