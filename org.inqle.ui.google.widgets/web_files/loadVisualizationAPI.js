3351e5a2c167ba59cd9845d746aa28c97eb52405
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2009-03-25 02:43:06 +0000

    bundle not working. having Javascript problems

---
+++ org.inqle.ui.google.widgets/web_files/loadVisualizationAPI.js
@@ -0,0 +1,4 @@
+alert('loadVisualizationAPI.js...');
+google.load('visualization', '1', {'packages':['motionchart']});
+google.setOnLoadCallback(null);
+alert('loaded Google visualization, package MotionChart');
