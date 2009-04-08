5cb630bd17a18f9b40be0d36f09bf15f0e57c003
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2009-04-08 23:37:23 +0000

    MotionChart works w/ demo data

--- org.inqle.ui.rap/src/main/org/inqle/ui/rap/Part.java
+++ org.inqle.ui.rap/src/main/org/inqle/ui/rap/Part.java
@@ -14,6 +14,7 @@ import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
 import org.inqle.ui.rap.tree.IDeltaListener;
 import org.inqle.ui.rap.tree.NullDeltaListener;
+import org.inqle.ui.rap.views.ObjectViewer;
 
 public abstract class Part implements IPart {
 
