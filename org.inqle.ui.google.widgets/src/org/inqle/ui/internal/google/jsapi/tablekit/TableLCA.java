3b17ea2b09c773466be1f6f96a299686b01b61a0
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2009-04-28 03:12:51 +0000

    widget events received by LCA.

--- org.inqle.ui.google.widgets/src/org/inqle/ui/internal/google/jsapi/tablekit/TableLCA.java
+++ org.inqle.ui.google.widgets/src/org/inqle/ui/internal/google/jsapi/tablekit/TableLCA.java
@@ -2,16 +2,20 @@ package org.inqle.ui.internal.google.jsapi.tablekit;
 
 import java.io.IOException;
 
+import org.apache.log4j.Logger;
 import org.eclipse.rwt.lifecycle.ControlLCAUtil;
 import org.eclipse.rwt.lifecycle.JSWriter;
 import org.eclipse.rwt.lifecycle.WidgetLCAUtil;
 import org.eclipse.rwt.lifecycle.WidgetUtil;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Widget;
 import org.inqle.ui.google.jsapi.Table;
 import org.inqle.ui.google.jsapi.VisualizationWidgetLCA;
 
 public class TableLCA extends VisualizationWidgetLCA {
 
+  private static final Logger log = Logger.getLogger(TableLCA.class);
+  
   /*
    * Initial creation procedure of the widget
    */
@@ -27,9 +31,11 @@ public class TableLCA extends VisualizationWidgetLCA {
   }
   
   public void readData( final Widget widget ) {
+    log.info( "TableLCA.readData(): widget=" + widget);
     Table table = ( Table )widget;
     String selectedItem = WidgetLCAUtil.readPropertyValue( table, "selectedItem" );
     table.setSelectedItem( selectedItem );
+    log.info( "TableLCA.readData(): selectedItem=" + selectedItem);
     ControlLCAUtil.processSelection( table, null, true );
   }
 }
