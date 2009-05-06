7834ae5c6b61d2a9033a4dce9cf7d794a7513f82
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2009-05-06 21:05:45 +0000

    added barchart, gague, geomap

---
+++ trunk/org.inqle.ui.google.widgets/src/org/inqle/ui/google/jsapi/Gauge.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * Copyright (c) 2002-2008 Innoopract Informationssysteme GmbH.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Innoopract Informationssysteme GmbH - initial API and implementation
+ ******************************************************************************/
+package org.inqle.ui.google.jsapi;
+
+import org.eclipse.swt.widgets.Composite;
+
+/**
+ * Renders a Google Visualization Gauge.
+ * 
+ * Note that this widget is rendered upon calling the setWidgetData method.  
+ * So if you wish to set options like width, height, colors, etc., you must do this 
+ * using method setWidgetOptions, before calling setWidgetData.
+ * 
+ * Usage:<code>
+ * 
+   </code>
+ * @See http://code.google.com/apis/visualization/documentation/gallery/columnchart.html
+ * @author David Donohue
+ * 2009/4/8
+ */
+public class Gauge extends VisualizationWidget {
+
+  public Gauge( final Composite parent, final int style ) {
+    super( parent, style );
+  }
+
+}
---
+++ trunk/org.inqle.ui.google.widgets/src/org/inqle/ui/google/jsapi/GaugeResource.java
@@ -0,0 +1,9 @@
+package org.inqle.ui.google.jsapi;
+
+public class GaugeResource extends GoogleVisualizationResource {
+  
+  public String getLocation() {
+    return "org/inqle/ui/google/jsapi/Gauge.js";
+  }
+  
+}
