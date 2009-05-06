8e2ce6d7448994d090a4dd45e4b8d182c0b72b88
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2009-05-06 21:05:45 +0000

    added barchart, gague, geomap

---
+++ org.inqle.ui.google.widgets/src/org/inqle/ui/google/jsapi/BarChartResource.java
@@ -0,0 +1,9 @@
+package org.inqle.ui.google.jsapi;
+
+public class BarChartResource extends GoogleVisualizationResource {
+  
+  public String getLocation() {
+    return "org/inqle/ui/google/jsapi/BarChart.js";
+  }
+  
+}
---
+++ org.inqle.ui.google.widgets/src/org/inqle/ui/google/jsapi/GeomapResource.java
@@ -0,0 +1,9 @@
+package org.inqle.ui.google.jsapi;
+
+public class GeomapResource extends GoogleVisualizationResource {
+  
+  public String getLocation() {
+    return "org/inqle/ui/google/jsapi/Geomap.js";
+  }
+  
+}
