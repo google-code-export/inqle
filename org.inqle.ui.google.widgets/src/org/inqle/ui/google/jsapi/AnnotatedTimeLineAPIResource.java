21157175ee6a8c9fc93657df2f0bb84691224b6e
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2009-05-06 03:53:26 +0000

    annotated timeline working i think

---
+++ org.inqle.ui.google.widgets/src/org/inqle/ui/google/jsapi/AnnotatedTimeLineAPIResource.java
@@ -0,0 +1,9 @@
+package org.inqle.ui.google.jsapi;
+
+public class AnnotatedTimeLineAPIResource extends GoogleVisualizationResource {
+
+  public String getLocation() {
+    return "org/inqle/ui/google/jsapi/AnnotatedTimeLineAPI.js";
+  }
+  
+}
