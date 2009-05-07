1dc2f9c14b8a9786309f109f3acf2b2829380908
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2009-05-07 03:13:07 +0000

    added IntensityMap but it displays in upper left only

---
+++ org.inqle.ui.google.widgets/src/org/inqle/ui/internal/google/jsapi/intensitymapkit/IntensityMapLCA.java
@@ -0,0 +1,37 @@
+/*******************************************************************************
+ * Copyright (c) 2002-2008 Innoopract Informationssysteme GmbH.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Innoopract Informationssysteme GmbH - initial API and implementation
+ ******************************************************************************/
+package org.inqle.ui.internal.google.jsapi.intensitymapkit;
+
+import java.io.IOException;
+
+import org.eclipse.rwt.lifecycle.ControlLCAUtil;
+import org.eclipse.rwt.lifecycle.JSWriter;
+import org.eclipse.rwt.lifecycle.WidgetUtil;
+import org.eclipse.swt.widgets.Widget;
+import org.inqle.ui.google.jsapi.IntensityMap;
+import org.inqle.ui.google.jsapi.VisualizationWidgetLCA;
+
+public class IntensityMapLCA extends VisualizationWidgetLCA {
+
+  /*
+   * Initial creation procedure of the widget
+   */
+  public void renderInitialization( final Widget widget ) throws IOException {
+    JSWriter writer = JSWriter.getWriterFor( widget );
+    String id = WidgetUtil.getId( widget );
+    writer.newWidget( "org.inqle.ui.google.jsapi.IntensityMap", new Object[]{
+      id
+    } );
+    writer.set( "appearance", "composite" );
+    writer.set( "overflow", "hidden" );
+    ControlLCAUtil.writeStyleFlags( ( IntensityMap )widget );
+  }
+}
