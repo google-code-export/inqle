df1bc39fa020d629a3addab5c5b915dc0ba0f60c
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2009-05-23 23:15:41 +0000

    removing unneeded web_files directory

--- org.inqle.ui.google.widgets/web_files/loadVisualizationAPI.js
+++
@@ -1,4 +0,0 @@
-alert('loadVisualizationAPI.js...');
-google.load('visualization', '1', {'packages':['motionchart']});
-//google.setOnLoadCallback(null);
-alert('finished calling google.load() for package: motionchart');
