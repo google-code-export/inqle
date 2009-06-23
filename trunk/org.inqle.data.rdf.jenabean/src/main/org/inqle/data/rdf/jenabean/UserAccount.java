64b22106942315ee66e4341655e2afdab93472d9
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2009-06-23 14:11:16 +0000

    removing PerssiterInitializer

--- trunk/org.inqle.data.rdf.jenabean/src/main/org/inqle/data/rdf/jenabean/UserAccount.java
+++ trunk/org.inqle.data.rdf.jenabean/src/main/org/inqle/data/rdf/jenabean/UserAccount.java
@@ -1,27 +1,28 @@
 package org.inqle.data.rdf.jenabean;
 
+import org.inqle.data.rdf.RDF;
+
 import thewebsemantic.Id;
 import thewebsemantic.Namespace;
-import org.inqle.data.rdf.RDF;
 
 @Namespace(RDF.INQLE)
 public class UserAccount {
 
-        private String userName;
-        private String password;
-        
-        @Id
-        public String getUserName() {
-                return userName;
-        }
-        public void setUserName(String userName) {
-                this.userName = userName;
-        }
-        public String getPassword() {
-                return password;
-        }
-        public void setPassword(String password) {
-                this.password = password;
-        }
+	private String userName;
+	private String password;
+	
+	@Id
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 }
