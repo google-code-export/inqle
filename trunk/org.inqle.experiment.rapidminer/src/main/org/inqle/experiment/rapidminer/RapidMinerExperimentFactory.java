e1c7f93d4cf1e3d50c50db32299612e6bef44c00
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2009-07-26 03:06:13 +0000

    compile errors present removing LearningCycle, etc.

--- trunk/org.inqle.experiment.rapidminer/src/main/org/inqle/experiment/rapidminer/RapidMinerExperimentFactory.java
+++ trunk/org.inqle.experiment.rapidminer/src/main/org/inqle/experiment/rapidminer/RapidMinerExperimentFactory.java
@@ -6,7 +6,7 @@ import org.inqle.core.util.InqleInfo;
 public class RapidMinerExperimentFactory {
 
 	public static IRapidMinerExperiment createRapidMinerExperiment(IExtensionSpec extensionSpec) {
-		RapidMinerExperiment experiment = new RapidMinerExperiment();
+		ClassificationRegressionCrossValidationExperiment experiment = new ClassificationRegressionCrossValidationExperiment();
 		experiment.setId(extensionSpec.getAttribute(InqleInfo.ID_ATTRIBUTE));
 		experiment.setExperimentClassPath(extensionSpec.getAttribute(InqleInfo.FILE_ATTRIBUTE));
 		experiment.setName(extensionSpec.getAttribute(IRapidMinerExperiment.NAME_ATTRIBUTE));
