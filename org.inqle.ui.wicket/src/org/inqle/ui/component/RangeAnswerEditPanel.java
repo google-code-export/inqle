6151686bdd04f7e12ab07fead1b31231f3d7ee35
reiern70 <reiern70@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2009-10-19 15:24:32 +0000

    range edit panel

--- org.inqle.ui.wicket/src/org/inqle/ui/component/RangeAnswerEditPanel.java
+++ org.inqle.ui.wicket/src/org/inqle/ui/component/RangeAnswerEditPanel.java
@@ -3,22 +3,30 @@
  */
 package org.inqle.ui.component;
 
-import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.markup.html.form.TextField;
 import org.inqle.ui.model.RangeAnswer;
 
 /**
  * @author Ernesto Reinaldo Barreiro (reiern70@gmail.com)
  *
  */
-public class RangeAnswerEditPanel<T extends Number> extends Panel {
+public class RangeAnswerEditPanel<T extends Number> extends AnswerEditingPanel<RangeAnswer<T>> {
 
 	private static final long serialVersionUID = 1L;
 
 	/**
 	 * @param id
 	 */
-	public RangeAnswerEditPanel(String id, RangeAnswer<Number> rangeAnswer) {
-		super(id);
+	public RangeAnswerEditPanel(String id, RangeAnswer<T> rangeAnswer) {
+		super(id, rangeAnswer);		
+					
+		TextField<T> minimumResponse = new TextField<T>("minimumResponse");		
+		minimumResponse.setRequired(true);
+		add(minimumResponse);
+		
+		TextField<T> maximumResponse = new TextField<T>("maximumResponse");		
+		minimumResponse.setRequired(true);
+		add(maximumResponse);		
 	}
 
 }
