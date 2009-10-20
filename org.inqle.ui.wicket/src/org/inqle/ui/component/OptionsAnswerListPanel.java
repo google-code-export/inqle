ddd0140ceb881535801203c32589ade09196e46f
reiern70 <reiern70@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2009-10-20 17:41:43 +0000

    OptionsAnswers

--- org.inqle.ui.wicket/src/org/inqle/ui/component/OptionsAnswerListPanel.java
+++ org.inqle.ui.wicket/src/org/inqle/ui/component/OptionsAnswerListPanel.java
@@ -3,6 +3,7 @@
  */
 package org.inqle.ui.component;
 
+import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.inqle.ui.model.Option;
 import org.inqle.ui.model.OptionsAnswer;
@@ -27,9 +28,11 @@ public class OptionsAnswerListPanel extends Panel {
 		super(id);
 		this.optionsAnswer = optionsAnswer;
 		
+		Form<Option> form = new Form<Option>("form");
+		add(form);
 		TableModel<Option> tableModel = new TableModel<Option>(Option.class, "id", "translationKey");
 		Table<Option> optionList = new Table<Option>("optionList", tableModel, optionsAnswer.getOptions());
-		add(optionList);
+		form.add(optionList);
 	}
 
 	public OptionsAnswer getOptionsAnswer() {
