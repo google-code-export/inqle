7dc08084d3d284e5cbabcd394cb55549451cac3a
reiern70 <reiern70@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2009-10-20 13:31:07 +0000

    range edit panel

---
+++ trunk/org.inqle.ui.wicket/src/org/inqle/ui/resources/Styles.java
@@ -0,0 +1,12 @@
+package org.inqle.ui.resources;
+
+import org.apache.wicket.ResourceReference;
+
+/**
+ * @author  Ernesto Reinaldo Barreiro (reiern70@gmail.com)
+ *
+ */
+public class Styles {
+
+	public static final ResourceReference MAIN_CSS = new ResourceReference(Styles.class, "main.css");
+}
--- trunk/org.inqle.ui.wicket/src/org/inqle/ui/wicket/Index.java
+++ trunk/org.inqle.ui.wicket/src/org/inqle/ui/wicket/Index.java
@@ -3,9 +3,11 @@
  */
 package org.inqle.ui.wicket;
 
+import org.apache.wicket.markup.html.CSSPackageResource;
 import org.apache.wicket.markup.html.WebPage;
 import org.inqle.ui.component.RangeAnswerEditPanel;
 import org.inqle.ui.model.RangeAnswer;
+import org.inqle.ui.resources.Styles;
 
 /**
  * @author Ernesto Reinaldo Barreiro (reiern70@gmail.com)
@@ -17,6 +19,7 @@ public class Index extends WebPage {
 	 * 
 	 */
 	public Index() {
+		add(CSSPackageResource.getHeaderContribution(Styles.MAIN_CSS));
 		rangeAnswer = new RangeAnswer<Float>();
 		RangeAnswerEditPanel<Float> testEdit = new RangeAnswerEditPanel<Float>("testEdit", rangeAnswer, Float.class);
 		add(testEdit);
