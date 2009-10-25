d6a4e1b53a64e3f89e10e6c59f1ee8825f1217a9
reiern70 <reiern70@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2009-10-25 15:56:23 +0000

    Question editing

--- org.inqle.ui.wicket/src/org/inqle/ui/dao/IDao.java
+++ org.inqle.ui.wicket/src/org/inqle/ui/dao/IDao.java
@@ -4,50 +4,13 @@
 package org.inqle.ui.dao;
 
 import java.io.Serializable;
-import java.util.List;
 
-import com.antilia.common.query.IQuery;
+import com.antilia.common.dao.IQuerableUpdatableDao;
 
 /**
  * 
  * @author  Ernesto Reinaldo Barreiro (reiern70@gmail.com)
  */
-public interface IDao<E extends Serializable> extends Serializable {
+public interface IDao<E extends Serializable> extends IQuerableUpdatableDao<E> {
 
-	/**
-	 * 
-	 * @param option
-	 * @return
-	 */
-	public E persist(E bean);
-	
-	
-	/**
-	 * 
-	 * @param option
-	 * @return
-	 */
-	public E update(E bean);
-	
-	
-	/**
-	 * 
-	 * @param option
-	 * @return
-	 */
-	public E delete(E option);
-	
-	/**
-	 * 
-	 * @param query
-	 * @return
-	 */
-	public List<E> getAllMatching(IQuery<E> query);
-	
-	/**
-	 * 
-	 * @param query
-	 * @return
-	 */
-	public Long countAllMatching(IQuery<E> query);
 }
