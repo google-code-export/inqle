78b974b5098cd190949711f5cca83fccfde46a9b
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2009-11-01 11:28:25 +0000

    changed Persister, to handles databases & datamodels in less centralized fashion

--- org.inqle.data.rdf.jenabean/src/main/org/inqle/data/rdf/jena/DBConnectorFactory.java
+++ org.inqle.data.rdf.jenabean/src/main/org/inqle/data/rdf/jena/DBConnectorFactory.java
@@ -4,21 +4,21 @@ import org.inqle.data.rdf.jena.tdb.TDBConnector;
 
 public class DBConnectorFactory {
 	
-	/**
-	 * Get the database connector.  Assume it should be a TDBConnector, unless the IDatabase is an
-	 * instance of class SDBDatabase (i.e. represents an SDB database)
-	 * @param databaseId
-	 * @return
-	 */
-	public static IDBConnector getDBConnector(IDatabase database) {
-		if (database==null) {
-			return getDBConnector();
-		}
-//		if (database instanceof SDBDatabase) {
-//			return new SDBConnector((SDBDatabase)database);
+//	/**
+//	 * Get the database connector.  Assume it should be a TDBConnector, unless the IDatabase is an
+//	 * instance of class SDBDatabase (i.e. represents an SDB database)
+//	 * @param databaseId
+//	 * @return
+//	 */
+//	public static IDBConnector getDBConnector(IDatabase database) {
+//		if (database==null) {
+//			return getDBConnector();
 //		}
-		return new TDBConnector(database.getId());
-	}
+////		if (database instanceof SDBDatabase) {
+////			return new SDBConnector((SDBDatabase)database);
+////		}
+//		return new TDBConnector(database.getId());
+//	}
 	
 	/**
 	 * Get the default database connector
