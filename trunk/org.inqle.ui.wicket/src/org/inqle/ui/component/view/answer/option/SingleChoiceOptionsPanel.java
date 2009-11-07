b2e81031af948b9c10c580b2f37cb0db4dc25098
reiern70 <reiern70@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2009-11-07 19:46:03 +0000

    Questions editing

--- trunk/org.inqle.ui.wicket/src/org/inqle/ui/component/view/answer/option/SingleChoiceOptionsPanel.java
+++ trunk/org.inqle.ui.wicket/src/org/inqle/ui/component/view/answer/option/SingleChoiceOptionsPanel.java
@@ -34,13 +34,17 @@ public class SingleChoiceOptionsPanel extends Panel {
 	
 	private Option selected;
 	
+	private List<CheckBoxLink> checkBoxes;
+	
 	public static class CheckBoxLink extends AbstractLink {
 		
 		private Option option;
 		
-		public CheckBoxLink(String id, Option option) {			
+		public CheckBoxLink(String id, Option option, SingleChoiceOptionsPanel panel) {			
 			super(id);
 			this.option = option;
+			setOutputMarkupId(true);
+			panel.addCheckBox(this);
 		}
 		
 		private static final long serialVersionUID = 1L;
@@ -63,9 +67,7 @@ public class SingleChoiceOptionsPanel extends Panel {
 		protected void onClick(AjaxRequestTarget target) {
 			SingleChoiceOptionsPanel choiceOptionsPanel = findParent(SingleChoiceOptionsPanel.class);
 			choiceOptionsPanel.setSelected(this.option);
-			if(target != null) {
-				target.addComponent(choiceOptionsPanel);
-			}
+			choiceOptionsPanel.updateChectBoxes(target);
 		}
 		
 	}
@@ -76,6 +78,7 @@ public class SingleChoiceOptionsPanel extends Panel {
 	public SingleChoiceOptionsPanel(String id, SingleChoiceAnswer optionsAnswer) {
 		super(id);
 		this.optionsAnswer = optionsAnswer;
+		checkBoxes = new ArrayList<CheckBoxLink>();
 		setOutputMarkupId(true);
 		
 		List<ICellPopulator<Option>> columns = new ArrayList<ICellPopulator<Option>>();
@@ -85,7 +88,7 @@ public class SingleChoiceOptionsPanel extends Panel {
 
 			@Override
 			public void populateItem(Item<ICellPopulator<Option>> cellItem, String componentId, IModel<Option> rowModel) {
-				cellItem.add(new CheckBoxLink(componentId, rowModel.getObject()));
+				cellItem.add(new CheckBoxLink(componentId, rowModel.getObject(), SingleChoiceOptionsPanel.this));
 			}
 			
 			@Override
@@ -129,6 +132,18 @@ public class SingleChoiceOptionsPanel extends Panel {
 
 	public void setSelected(Option selected) {
 		this.selected = selected;
+	}
+	
+	public void updateChectBoxes(AjaxRequestTarget target) {
+		if(target != null) {
+			for(CheckBoxLink boxLink: checkBoxes) {
+				target.addComponent(boxLink);
+			}			
+		}
+	}
+	
+	public void addCheckBox(CheckBoxLink link) {
+		checkBoxes.add(link);
 	}
 
 }
