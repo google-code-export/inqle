946083b426bcf38c28117e14aff020c5b2469456
reiern70 <reiern70@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2009-11-13 20:57:24 +0000

    Questions editing

--- org.inqle.ui.wicket/src/org/inqle/ui/wicket/BasePage.java
+++ org.inqle.ui.wicket/src/org/inqle/ui/wicket/BasePage.java
@@ -7,6 +7,8 @@ import org.apache.wicket.markup.html.CSSPackageResource;
 import org.apache.wicket.markup.html.WebPage;
 import org.inqle.ui.resources.Styles;
 
+import com.antilia.web.resources.DefaultStyle;
+
 /**
  * @author Ernesto Reinaldo Barreiro (reiern70@gmail.com)
  */
@@ -17,6 +19,7 @@ public class BasePage extends WebPage {
 	 * 
 	 */
 	public BasePage() {
+		add(CSSPackageResource.getHeaderContribution(DefaultStyle.CSS_MAIN));
 		add(CSSPackageResource.getHeaderContribution(Styles.MAIN_CSS));	
 	}
 }
