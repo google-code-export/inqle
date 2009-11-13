9fc471f609c4cc3af67022d25c1a8ca7b86a4d63
reiern70 <reiern70@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2009-11-13 19:51:11 +0000

    Questions editing

---
+++ trunk/org.inqle.ui.wicket/src/org/inqle/ui/wicket/ViewPage.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.inqle.ui.wicket;
+
+
+/**
+ * @author Ernesto Reinaldo Barreiro (reiern70@gmail.com)
+ */
+public class ViewPage extends BasePage {
+
+	
+	/**
+	 * 
+	 */
+	public ViewPage() {
+		super();
+	}
+}
