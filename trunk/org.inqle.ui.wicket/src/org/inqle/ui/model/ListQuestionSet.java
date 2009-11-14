fa4707a7f9f34e0b71e3e163587356fe7a98a694
reiern70 <reiern70@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2009-11-14 09:25:47 +0000

    Questions editing

---
+++ trunk/org.inqle.ui.wicket/src/org/inqle/ui/model/ListQuestionSet.java
@@ -0,0 +1,42 @@
+package org.inqle.ui.model;
+
+import java.util.List;
+
+/**
+ * @author Ernesto Reinaldo Barreiro (reiern70@gmail.com)
+ *
+ */
+public class ListQuestionSet implements IQuestionsSet {
+
+	private static final long serialVersionUID = 1L;
+	
+	private List<Question> questions;
+	
+	private int current = -1;
+	
+	public ListQuestionSet(List<Question> questions) {
+		this.questions= questions;
+	}
+
+	@Override
+	public boolean hasNext() {
+		if(current < (questions.size()-1))
+			return  true;
+		return false;
+	}
+	
+	@Override
+	public boolean hasPrevious() {
+		return current > 0;
+	}
+	
+	@Override
+	public Question next() {
+		return this.questions.get(current++);
+	}
+	
+	@Override
+	public Question previous() {
+		return this.questions.get(current--);
+	}
+}
