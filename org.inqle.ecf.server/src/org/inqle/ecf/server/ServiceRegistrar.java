ac9e5187ffcab28ce8d2369089e9970a792b6b27
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2009-12-10 20:31:35 +0000

    renamed to org.inqle.ecf.server

---
+++ org.inqle.ecf.server/src/org/inqle/ecf/server/ServiceRegistrar.java
@@ -0,0 +1,37 @@
+package org.inqle.ecf.server;
+
+import java.util.Properties;
+
+import org.eclipse.ecf.osgi.services.distribution.IDistributionConstants;
+import org.inqle.qa.common.services.Hello;
+import org.inqle.qa.common.services.IHello;
+import org.inqle.qa.common.services.QAServiceConstants;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+/**
+ * Create a service registration for a remote OSGi service for INLQE QA application.
+ * Adds a property to the props, which identifies the URI of the server, so that the client can identify
+ * which server a service reference belongs to.
+ * @author David Donohue
+ * 2009/10/29
+ *
+ */
+public class ServiceRegistrar {
+
+	public static ServiceRegistration registerService(
+			BundleContext bundleContext, 
+			String containerId,
+			String serviceName, 
+			Object serviceObject) {
+		Properties props = new Properties();
+		// add OSGi service property indicating this
+		props.put(IDistributionConstants.REMOTE_INTERFACES, IDistributionConstants.REMOTE_INTERFACES_WILDCARD);
+		props.put(QAServiceConstants.PROPERTY_SERVER_URI, containerId);
+		
+		ServiceRegistration serviceRegistration = bundleContext.registerService(
+			serviceName, serviceObject, props);
+		return serviceRegistration;
+	}
+
+}
