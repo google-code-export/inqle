93dfc48c57f1b14e2fbb48d7be16b652eee6c114
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2009-12-22 16:40:30 +0000

    new ECF model, supporting multiple protocols per server and per service

--- org.inqle.ecf.common/src/org/inqle/ecf/common/EcfServiceConstants.java
+++ org.inqle.ecf.common/src/org/inqle/ecf/common/EcfServiceConstants.java
@@ -5,5 +5,6 @@ public class EcfServiceConstants {
 	/**
 	 * This property is added by ECF server to each service upon registering it
 	 */
-	public static final String PROPERTY_SERVER_URI = "org.inqle.ecf.serverUri";
+//	public static final String PROPERTY_SERVER_URI = "org.inqle.ecf.server.uri";
+//	public static final String PROPERTY_SERVER_PORT = "org.inqle.ecf.server.port";
 }
