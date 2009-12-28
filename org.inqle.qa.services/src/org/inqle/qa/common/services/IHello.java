7338fecf4ea9f81a8dd96f8285a08b8596c199d6
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2009-12-28 04:52:32 +0000

    this bundle fragment replaces org.inqle.qa.common

---
+++ org.inqle.qa.services/src/org/inqle/qa/common/services/IHello.java
@@ -0,0 +1,9 @@
+package org.inqle.qa.common.services;
+
+import org.inqle.ecf.common.IInqleEcfService;
+
+public interface IHello extends IInqleEcfService {
+
+	public String hello(String from);
+	
+}
