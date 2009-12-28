ad81a0906d084b5c77c04c1ee303dadccb04dbe5
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2009-12-28 04:53:48 +0000

    removing QA packages

--- trunk/org.inqle.ecf.common/src/org/inqle/qa/common/services2/IHello.java
+++
@@ -1,9 +0,0 @@
-package org.inqle.qa.common.services2;
-
-import org.inqle.ecf.common.IInqleEcfService;
-
-public interface IHello extends IInqleEcfService {
-
-	public String hello(String from);
-	
-}
