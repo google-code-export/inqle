307f6f2ad35aad3e613a82beee4272316db4e322
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2009-12-30 13:51:48 +0000

    QA object service registers and is discovered

--- trunk/org.inqle.qa.client/src/main/org/inqle/qa/common/activator/QAClientActivator.java
+++ trunk/org.inqle.qa.client/src/main/org/inqle/qa/common/activator/QAClientActivator.java
@@ -4,6 +4,7 @@ import org.apache.log4j.Logger;
 import org.inqle.ecf.client.Servicer;
 import org.inqle.ecf.services.IHello;
 import org.inqle.qa.ecf.services.IHello2;
+import org.inqle.qa.services.IQAObjectService;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 //import org.inqle.rdf.object.services.IRdfObjectService;
@@ -23,13 +24,13 @@ public class QAClientActivator implements BundleActivator{
 		IHello hello1 = servicer.getServiceObject(IHello.class, "ecftcp://localhost:3787/server1");
 		log.info("hello1.hello() to hello1 service object: " + hello1);
 		log.info(hello1.hello("QA Client (to ECF server #1, IHello1 service)"));
-		
+
 		IHello2 hello2 = servicer.getServiceObject(IHello2.class, "ecftcp://localhost:3787/server1");
 		log.info("hello2.hello()" + hello2.hello("QA Client (to ECF server #1, IHello2 service)"));
 		
-//		IRdfObjectService rdfObjectService = servicer.getServiceObject(
-//				IRdfObjectService.class, 
-//				"ecftcp://localhost:3787/server1");
+		IQAObjectService qaObjectService = servicer.getServiceObject(
+				IQAObjectService.class, 
+				"ecftcp://localhost:3787/server1");
 	}
 
 	public void stop(BundleContext context) throws Exception {
