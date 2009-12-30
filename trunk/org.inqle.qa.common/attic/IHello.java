db86c1c9d61e449c48ff85b7b2a75a7a317ae1d5
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2009-12-30 13:27:31 +0000

    replacing w/ new version

--- trunk/org.inqle.qa.common/attic/IHello.java
+++
@@ -1,9 +0,0 @@
-package org.inqle.qa.common.services;
-
-import org.inqle.ecf.common.IInqleEcfService;
-
-public interface IHello extends IInqleEcfService {
-
-	public String hello(String from);
-	
-}
