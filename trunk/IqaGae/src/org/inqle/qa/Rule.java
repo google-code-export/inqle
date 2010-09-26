56c6aa15c9d552a0d6ddc61db8c7fbf898adb58c
drdonohue@gmail.com <drdonohue@gmail.com@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2010-09-26 18:52:51 +0000

    added testing rules

--- trunk/IqaGae/src/org/inqle/qa/Rule.java
+++ trunk/IqaGae/src/org/inqle/qa/Rule.java
@@ -1,11 +1,19 @@
 package org.inqle.qa;
 
+import java.util.List;
+
 public class Rule {
 
 	private String entityKey;
 	private String id;
 	private String name;
 	private String description;
+	private List<Rule> andRules;
+	private List<Rule> orRules;
+	private String type;
+	private String property;
+	private String operator;
+	private String values;
 	
 	public void setName(String name) {
 		this.name = name;
@@ -30,5 +38,35 @@ public class Rule {
 	}
 	public void setId(String id) {
 		this.id = id;
+	}
+	public void setAndRules(List<Rule> andRules) {
+		this.andRules = andRules;
+	}
+	public void setOrRules(List<Rule> orRules) {
+		this.orRules = orRules;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getProperty() {
+		return property;
+	}
+	public void setProperty(String property) {
+		this.property = property;
+	}
+	public String getOperator() {
+		return operator;
+	}
+	public void setOperator(String operator) {
+		this.operator = operator;
+	}
+	public String getValues() {
+		return values;
+	}
+	public void setValues(String values) {
+		this.values = values;
 	}
 }
