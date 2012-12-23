14b7afb3d4b7c70dde847505abf3d285eba57839
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2012-12-23 07:56:56 +0000

    cutomizing QuestionController

--- qs/src/main/java/org/inqle/web/QuestionController.java
+++ qs/src/main/java/org/inqle/web/QuestionController.java
@@ -1,14 +1,30 @@
 package org.inqle.web;
 
+import java.util.List;
+
 import org.inqle.domain.Question;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.roo.addon.web.mvc.controller.json.RooWebJson;
 import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping("/questions")
 @Controller
 @RooWebScaffold(path = "questions", formBackingObject = Question.class)
 @RooWebJson(jsonObject = Question.class)
 public class QuestionController {
+	
+	@RequestMapping(value = "/ask", method = RequestMethod.GET, headers = "Accept=application/json")
+	public ResponseEntity<String> getQuestionsToAsk(@RequestParam String participantId) {
+	    HttpHeaders headers = new HttpHeaders();
+	    headers.add("Content-Type", "application/json");
+	    //TODO - figure out the questions to ask this participant
+	    List<Question> result = Question.findAllQuestions();
+	    return new ResponseEntity<String>(Question.toJsonArray(result), headers, HttpStatus.OK);
+	}
 }
