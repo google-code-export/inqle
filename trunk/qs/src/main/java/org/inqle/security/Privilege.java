d21c5056d7117263622bc56688231fb8fe30ad63
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2013-01-08 04:13:11 +0000

    hooked up QSAuthenticationProvider, to retrieve roles

---
+++ trunk/qs/src/main/java/org/inqle/security/Privilege.java
@@ -0,0 +1,7 @@
+package org.inqle.security;
+
+
+public enum Privilege {
+
+    ROLE_PARTICIPANT, ROLE_GUARDIAN, ROLE_INVESTIGATOR, ROLE_SUPER, ROLE_AUTHENTICATED;
+}
--- trunk/qs/src/main/java/org/inqle/web/security/QSAuthenticationProvider.java
+++ trunk/qs/src/main/java/org/inqle/web/security/QSAuthenticationProvider.java
@@ -5,9 +5,10 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityNotFoundException;
 import javax.persistence.NonUniqueResultException;
-import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 import org.inqle.domain.Account;
+import org.inqle.security.Privilege;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -33,21 +34,26 @@ UsernamePasswordAuthenticationToken authentication)
     }
 
     @Override
-    protected UserDetails retrieveUser(String username,
-UsernamePasswordAuthenticationToken
-authentication) throws AuthenticationException {
+    protected UserDetails retrieveUser(String username, UsernamePasswordAuthenticationToken authentication) throws AuthenticationException {
         String password = (String) authentication.getCredentials();
         if (!StringUtils.hasText(password)) {
             throw new BadCredentialsException("Please enter password");
         }
         List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
         try {
-//        	Query q = em.createQuery(
-//        			03.
-//        			"SELECT e FROM " + Role.class.getName() + " e WHERE userid = :userid");
-//        			q.setParameter("userid", date);
-//            Account account = em.find(Account.class, arg1);
-//            authorities.add(new GrantedAuthorityImpl("ROLE_USER"));
+        	TypedQuery<Account> accountQueries = Account.findAccountsByUsernameEqualsAndPasswordEquals(username, password);
+            List<Account> accounts = accountQueries.getResultList();
+        	if (accounts.size() == 0) {
+            	throw new BadCredentialsException("Invalid username or password");
+            }
+            if (accounts.size() > 1) {
+            	throw new BadCredentialsException("Non-unique user, contact administrator");
+            }
+            Account account = accounts.get(0);
+        	authorities.add(new GrantedAuthorityImpl(Privilege.ROLE_AUTHENTICATED.toString()));
+        	for (Privilege priv: account.getPrivs()) {
+        		authorities.add(new GrantedAuthorityImpl(priv.toString()));
+        	}
 //            Role role = Role.findRoles
         } catch (EmptyResultDataAccessException e) {
             throw new BadCredentialsException("Invalid username or password");
