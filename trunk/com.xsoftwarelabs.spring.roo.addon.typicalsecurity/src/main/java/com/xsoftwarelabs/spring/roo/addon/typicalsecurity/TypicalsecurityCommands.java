10f94d92cfed41784b85b85e41f65e0a624c587e
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2013-01-19 03:53:42 +0000

    version 0.2.0 mostly working

---
+++ trunk/com.xsoftwarelabs.spring.roo.addon.typicalsecurity/src/main/java/com/xsoftwarelabs/spring/roo/addon/typicalsecurity/TypicalsecurityCommands.java
@@ -0,0 +1,54 @@
+package com.xsoftwarelabs.spring.roo.addon.typicalsecurity;
+
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Reference;
+import org.apache.felix.scr.annotations.Service;
+import org.springframework.roo.model.JavaType;
+import org.springframework.roo.shell.CliAvailabilityIndicator;
+import org.springframework.roo.shell.CliCommand;
+import org.springframework.roo.shell.CliOption;
+import org.springframework.roo.shell.CommandMarker;
+
+/**
+ * Sample of a command class. The command class is registered by the Roo shell following an
+ * automatic classpath scan. You can provide simple user presentation-related logic in this
+ * class. You can return any objects from each method, or use the logger directly if you'd
+ * like to emit messages of different severity (and therefore different colours on 
+ * non-Windows systems).
+ * 
+ * @since 1.1
+ */
+@Component // Use these Apache Felix annotations to register your commands class in the Roo container
+@Service
+public class TypicalsecurityCommands implements CommandMarker { // All command types must implement the CommandMarker interface
+    
+    /**
+     * Get a reference to the TypicalsecurityOperations from the underlying OSGi container
+     */
+    @Reference private TypicalsecurityOperations operations;
+    
+    /**
+     * This method is optional. It allows automatic command hiding in situations when the command should not be visible.
+     * For example the 'entity' command will not be made available before the user has defined his persistence settings 
+     * in the Roo shell or directly in the project.
+     * 
+     * You can define multiple methods annotated with {@link CliAvailabilityIndicator} if your commands have differing
+     * visibility requirements.
+     * 
+     * @return true (default) if the command should be visible at this stage, false otherwise
+     */
+    @CliAvailabilityIndicator({"typicalsecurity setup"})
+    public boolean isCommandAvailable() {
+    	return true;
+    }
+    
+    /**
+     * This method registers a command with the Roo shell. It has no command attribute.
+     * 
+     */
+    @CliCommand(value = {"typicalsecurity setup"}, help = "Setup Typicalsecurity addon")
+    public void setup( @CliOption(key="entityPackage", mandatory=true, unspecifiedDefaultValue = "false") String entityPackage,
+    		 @CliOption(key="controllerPackage", mandatory=true, unspecifiedDefaultValue = "false") String controllerPackage) {
+        operations.setup(entityPackage, controllerPackage);
+    }
+}
