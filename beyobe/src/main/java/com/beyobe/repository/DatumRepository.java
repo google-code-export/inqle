5aafa48ded52df6def68d50b4a7aa199b98a575e
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2013-05-21 01:45:55 +0000

    website works no errors

--- beyobe/src/main/java/com/beyobe/repository/DatumRepository.java
+++ beyobe/src/main/java/com/beyobe/repository/DatumRepository.java
@@ -9,38 +9,38 @@ import org.springframework.roo.addon.layers.repository.jpa.RooJpaRepository;
 
 @RooJpaRepository(domainType = Datum.class)
 public interface DatumRepository {
-	/**
-	 * Get all latest answered data for this participant
-	 * @param participantId
-	 * @return
-	 */
-	@Query("select d from LatestParticipantDatum lpd, Datum d " +
-			" where lpd.participant.id=?1" +
-			" and lpd.datum.id = d.id" +
-			" order by lpd.created desc")
-	List<Datum> getLatestData(Integer participantId);
-	
-	/**
-	 * Get latest datum for this participant, by concept key
-	 * @param participantId
-	 * @return
-	 */
-	@Query("select d from LatestParticipantDatum lpd, Datum d " +
-			" where lpd.participant.id=?1" +
-			" and lpd.datum.id = d.id" +
-			" and lpd.concept.conceptkey = ?2" +
-			" order by lpd.created desc limit 1")
-	Datum getLatestDatum(Integer participantId, String conceptkey);
-	
-	/**
-	 * Get data by concept ID
-	 * @param participantId
-	 * @return
-	 */
-	@Query("select d from Datum d " +
-			" where d.participant.id = ?1" +
-			" and d.status >= 0 " +
-			" and d.concept.id = ?2" +
-			" order by d.created desc")
-	List<Datum> getDataByConcept(Integer participantId, Integer conceptId);
+//	/**
+//	 * Get all latest answered data for this participant
+//	 * @param participantId
+//	 * @return
+//	 */
+//	@Query("select d from LatestParticipantDatum lpd, Datum d " +
+//			" where lpd.participant.id=?1" +
+//			" and lpd.datum.id = d.id" +
+//			" order by lpd.created desc")
+//	List<Datum> getLatestData(Integer participantId);
+//	
+//	/**
+//	 * Get latest datum for this participant, by concept key
+//	 * @param participantId
+//	 * @return
+//	 */
+//	@Query("select d from LatestParticipantDatum lpd, Datum d " +
+//			" where lpd.participant.id=?1" +
+//			" and lpd.datum.id = d.id" +
+//			" and lpd.concept.conceptkey = ?2" +
+//			" order by lpd.created desc limit 1")
+//	Datum getLatestDatum(Integer participantId, String conceptkey);
+//	
+//	/**
+//	 * Get data by concept ID
+//	 * @param participantId
+//	 * @return
+//	 */
+//	@Query("select d from Datum d " +
+//			" where d.participant.id = ?1" +
+//			" and d.status >= 0 " +
+//			" and d.concept.id = ?2" +
+//			" order by d.created desc")
+//	List<Datum> getDataByConcept(Integer participantId, Integer conceptId);
 }
--- beyobe/src/main/java/com/beyobe/repository/QuestionRepository.java
+++ beyobe/src/main/java/com/beyobe/repository/QuestionRepository.java
@@ -24,81 +24,82 @@ public interface QuestionRepository {
 			)
 	List<Question> getSubscribedQuestions(Long participantId);
 	
-	/**
-	 * Get a list of questions to which this participant has subscribed, that have not
-	 * been answered too recently
-	 * @param participantId
-	 * @return list of questions, ordered by question priority then creation date
-	 */
-	@Query("select distinct q from Question q, Subscription s " +
-			" where s.participant.id=?1 and s.question.id = q.id" +
-			" and not exists" +
-				" (select lpd.id from LatestParticipantDatum lpd" +
-				" where lpd.participant.id=?1 and lpd.datum.question.id = q.id" +
-				" and lpd.askableAfter > current_timestamp())" +
-			" order by q.priority asc, s.created desc "
-			)
-	List<Question> getAvailableSubscribedQuestions(Long participantId);
-	
-	/**
-	 * Get all questions to which this participant has not subscribed
-	 * @param participantId
-	 * @return list of questions, ordered by question priority
-	 */
-	@Query("select distinct q from Question q where not exists" +
-		" (select s.id from Subscription s" +
-		" where s.participant.id=?1 and s.question.id = q.id)" +
-		" order by q.priority asc")
-	List<Question> getUnsubscribedQuestions(Long participantId);
-	
-	/**
-	 * Get all questions to which this participant has not subscribed
-	 * @param participantId
-	 * @return list of questions, ordered by question priority
-	 */
-	@Query("select distinct q from Question q where q.lang = ?2 and not exists" +
-		" (select s.id from Subscription s" +
-		" where s.participant.id=?1 and s.question.id = q.id)" +
-		" order by q.priority asc")
-	List<Question> getUnsubscribedQuestionsOfLanguage(Long participantId, String lang);
-	
-	/**
-	 * Get a list of questions that this participant has never answered
-	 * @param participantId
-	 * @return list of questions, ordered by question priority
-	 */
-	@Query("select distinct q from Question q where not exists" +
-		" (select d.id from Datum d" +
-		" where d.participant.id=?1 and d.question.id = q.id)" +
-		" order by q.priority asc")
-	List<Question> getNeveransweredQuestions(Long participantId);
-	
-	/**
-	 * Get all questions that have not already been answered within the question's 
-	 * latency period
-	 * @param participantId
-	 * @return list of questions, ordered by question priority
-	 */
-	@Query("select distinct q from Question q" +
-		" where not exists" +
-		" (select lpd.id from LatestParticipantDatum lpd" +
-		" where lpd.participant.id=?1 and lpd.datum.question.id = q.id" +
-		" and lpd.askableAfter > current_timestamp())" +
-		" order by q.priority asc")
-	List<Question> getAvailableQuestions(Long participantId);
-	
-	/**
-	 * Get all questions that have not already been answered within the question's 
-	 * latency period
-	 * @param participantId
-	 * @return list of questions, ordered by question priority
-	 */
-	@Query("select distinct q from Question q" +
-		" where q.lang = ?2 and not exists" +
-		" (select lpd.id from LatestParticipantDatum lpd" +
-		" where lpd.participant.id=?1 and lpd.datum.question.id = q.id" +
-		" and lpd.askableAfter > current_timestamp())" +
-		" order by q.priority asc")
-	List<Question> getAvailableQuestionsOfLanguage(Long participantId, String lang);
+
+//	/**
+//	 * Get a list of questions to which this participant has subscribed, that have not
+//	 * been answered too recently
+//	 * @param participantId
+//	 * @return list of questions, ordered by question priority then creation date
+//	 */
+//	@Query("select distinct q from Question q, Subscription s " +
+//			" where s.participant.id=?1 and s.question.id = q.id" +
+//			" and not exists" +
+//				" (select lpd.id from LatestParticipantDatum lpd" +
+//				" where lpd.participant.id=?1 and lpd.datum.question.id = q.id" +
+//				" and lpd.askableAfter > current_timestamp())" +
+//			" order by q.priority asc, s.created desc "
+//			)
+//	List<Question> getAvailableSubscribedQuestions(Long participantId);
+//	
+//	/**
+//	 * Get all questions to which this participant has not subscribed
+//	 * @param participantId
+//	 * @return list of questions, ordered by question priority
+//	 */
+//	@Query("select distinct q from Question q where not exists" +
+//		" (select s.id from Subscription s" +
+//		" where s.participant.id=?1 and s.question.id = q.id)" +
+//		" order by q.priority asc")
+//	List<Question> getUnsubscribedQuestions(Long participantId);
+//	
+//	/**
+//	 * Get all questions to which this participant has not subscribed
+//	 * @param participantId
+//	 * @return list of questions, ordered by question priority
+//	 */
+//	@Query("select distinct q from Question q where q.lang = ?2 and not exists" +
+//		" (select s.id from Subscription s" +
+//		" where s.participant.id=?1 and s.question.id = q.id)" +
+//		" order by q.priority asc")
+//	List<Question> getUnsubscribedQuestionsOfLanguage(Long participantId, String lang);
+//	
+//	/**
+//	 * Get a list of questions that this participant has never answered
+//	 * @param participantId
+//	 * @return list of questions, ordered by question priority
+//	 */
+//	@Query("select distinct q from Question q where not exists" +
+//		" (select d.id from Datum d" +
+//		" where d.participant.id=?1 and d.question.id = q.id)" +
+//		" order by q.priority asc")
+//	List<Question> getNeveransweredQuestions(Long participantId);
+//	
+//	/**
+//	 * Get all questions that have not already been answered within the question's 
+//	 * latency period
+//	 * @param participantId
+//	 * @return list of questions, ordered by question priority
+//	 */
+//	@Query("select distinct q from Question q" +
+//		" where not exists" +
+//		" (select lpd.id from LatestParticipantDatum lpd" +
+//		" where lpd.participant.id=?1 and lpd.datum.question.id = q.id" +
+//		" and lpd.askableAfter > current_timestamp())" +
+//		" order by q.priority asc")
+//	List<Question> getAvailableQuestions(Long participantId);
+//	
+//	/**
+//	 * Get all questions that have not already been answered within the question's 
+//	 * latency period
+//	 * @param participantId
+//	 * @return list of questions, ordered by question priority
+//	 */
+//	@Query("select distinct q from Question q" +
+//		" where q.lang = ?2 and not exists" +
+//		" (select lpd.id from LatestParticipantDatum lpd" +
+//		" where lpd.participant.id=?1 and lpd.datum.question.id = q.id" +
+//		" and lpd.askableAfter > current_timestamp())" +
+//		" order by q.priority asc")
+//	List<Question> getAvailableQuestionsOfLanguage(Long participantId, String lang);
 	
 }
