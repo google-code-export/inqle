e385d3ff408a35505eb342c888e8e39c22a08349
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2013-05-26 08:59:16 +0000

    tagtheday gwt app talking to spring beyobe server

--- tagtheday/src/main/java/com/beyobe/client/Constants.java
+++ tagtheday/src/main/java/com/beyobe/client/Constants.java
@@ -6,7 +6,7 @@ import com.google.gwt.i18n.client.DateTimeFormat;
 public class Constants {
 
 	public static final String FORMAT_DATE_TIME = "yyyy-MM-dd HH:mm:SS";
-	public static final String BASEURL_BEYOBE_SERVICE = "http://localhost:8080/beyobe/service/";
+	public static final String BASEURL_BEYOBE_SERVICE = "http://127.0.0.1:8080/beyobe/service/";
 	public static final int TIMEOUT_LOGIN = 4000;
 	public static final int TIMEOUT_DATUM_CLIENT = 10000;
 	public static DateTimeFormat DAY_FORMATTER = DateTimeFormat.getFormat("yyyy-MM-dd");
