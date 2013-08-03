90a3b101276abbaef17236591029205ea0f252e4
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2013-08-03 03:11:11 +0000

    adding Directives adding Unsubscribe event

--- tagtheday/src/main/java/com/beyobe/client/beans/Parcel.java
+++ tagtheday/src/main/java/com/beyobe/client/beans/Parcel.java
@@ -3,12 +3,9 @@ package com.beyobe.client.beans;
 import java.util.List;
 
 public interface Parcel {
-	public List<Question> getQuestionQueue();
-	public void setQuestionQueue(List<Question> questions);
-	public List<Question> getOtherKnownQuestions();
-	public void setOtherKnownQuestions(List<Question> questions);
-	public List<Datum> getData();
-	public void setData(List<Datum> data);
+	public Directive getDirective();
+	public void setDirective(Directive directive);
+	
 	public Participant getParticipant();
 	public void setParticipant(Participant participant);
 	public String getSessionToken();
@@ -17,14 +14,26 @@ public interface Parcel {
 	public void setUsername(String userName);
 	public String getPassword();
 	public void setPassword(String text);
+	
+//	public List<Question> getQuestionQueue();
+//	public void setQuestionQueue(List<Question> questions);
+	
+	public List<Question> getOtherKnownQuestions();
+	public void setOtherKnownQuestions(List<Question> questions);
+	
 	public Question getQuestion();
 	public void setQuestion(Question question);
+	public List<Question> getQuestions();
+	public void setQuestions(List<Question> questions);
+	
 	public Datum getDatum();
 	public void setDatum(Datum datum);
+	public List<Datum> getData();
+	public void setData(List<Datum> data);
+	
 	public Message getMessage();
 	public void setMessage(Message message);
 	public String getQueryTerm();
 	public void setQueryTerm(String queryTerm);
-	public List<Question> getQuestions();
-	public void setQuestions(List<Question> questions);
+	
 }
