8351a5b9e1ad17150f9762222c0a993101e13ddf
drdonohue <drdonohue@63d3e388-e54a-0410-bb43-9fafe7d581bf>
2013-08-24 11:07:42 +0000

    switched to MySQL persistence

--- tagtheday/src/main/java/com/beyobe/client/Constants.java
+++ tagtheday/src/main/java/com/beyobe/client/Constants.java
@@ -4,10 +4,15 @@ import com.google.gwt.i18n.client.DateTimeFormat;
 
 
 public class Constants {
-	public static final String CLIENT = "beyobe.ttd.1";
-	
-	public static final String FORMAT_DATE_TIME = "yyyy-MM-dd HH:mm:SS";
 	public static final String BASEURL_BEYOBE_SERVICE = "http://127.0.0.1:8080/beyobe/service/";
+	public static final String CLIENT = "tagtheday";
+	public static final String CLIENT_VERSION = "20130824-1";
+	public static final Integer DEFAULT_TEXTFIELD_MAX_LENGTH = 50;
+	public static final Integer DEFAULT_MEMOFIELD_MAX_LENGTH = 2000;
+	public static final String FORMAT_DATE_TIME = "yyyy-MM-dd HH:mm:SS";
+	
+	public static final int MAXLENGTH_BUTTONLABEL = 14;
+	public static final int MAXLENGTH_BUTTONTITLE = 40;
 	
 	public static final int TIMEOUT_LOGIN = 4000;
 	public static final int TIMEOUT_PARCEL_CLIENT = 10000;
@@ -21,20 +26,14 @@ public class Constants {
 	public static final String SERVERACTION_LOGIN = "loginDrupal";
 	public static final String SERVERACTION_STORE_QUESTION = "storeQuestion";
 	public static final String SERVERACTION_STORE_DATUM = "storeDatum";
-	public static final Integer DEFAULT_TEXTFIELD_MAX_LENGTH = 50;
-	public static final Integer DEFAULT_MEMOFIELD_MAX_LENGTH = 2000;
-	public static final int TIMEOUT_SIGNUP = 10000;
+//	public static final int TIMEOUT_SIGNUP = 10000;
 	public static final String SERVERACTION_SIGNUP = "signup";
 	public static final String SERVERACTION_TESTUSERNAME = "testUsername";
 	public static final int MINIMUM_PASSWORD_LENGTH = 1;
 	public static final String SERVERACTION_SEARCH_QUESTIONS = "searchForQuestions";
 	public static final String SERVERACTION_UNSUBSCRIBE = "unsubscribe";
-
 	public static final String SERVERACTION_SAVE_UNSAVED = "saveUnsaved";
 
 	public static final String URL_SIGNUP = "http://localhost:8082/user/register";
 
-	public static final int MAXLENGTH_BUTTONLABEL = 14;
-	public static final int MAXLENGTH_BUTTONTITLE = 40;
-	
 }
